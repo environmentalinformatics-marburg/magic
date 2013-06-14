3742bc1f674324459f21423fc46525465e4e3ac4
Florian Detsch <florian.detsch@staff.uni-marburg.de>
2013-06-14 10:55:38 +0200

    Update: additional figures

--- figures/paper_kuehnlein_2013/figure_14.R
+++ figures/paper_kuehnlein_2013/figure_14.R
@@ -1,91 +1,176 @@
-library(raster)
 library(latticeExtra)
-library(fields)
+library(sm)
+library(RColorBrewer)
+library(grid)
+## Einlesen der Daten
+inpath <- "/media/PRECI/slalom_vergleich/sandbox/slalom_vs_mod06/validation/atlantiv_coefficients_feb/figures/figure_14"
+dat <- read.table(paste(inpath, "dat.txt", sep = "/"), header=T, na.strings="-99.000000")
 
-inpath <- "C:/tappelhans/uni/marburg/colleagues/meike/article/figure_processing/figure_14/new"
-#flist <- list.files(inpath, pattern = "2", recursive = T)
-#
-#newnames <- tolower(flist)
-#file.rename(paste(inpath, flist, sep = "/"), paste(inpath, newnames, sep = "/"))
+#attach(dat)
+#dat <- subset(dat_read, MSGTau < 80)
+#dat <- subset(dat, MSGTau > 5 & CloudTau > 5)
+#dat <- subset(dat, Anz > 2)
 
 
-aef75 <- raster(paste(inpath, "200806280945_py75_mask_tau_gt_05.rst", sep = "/"),
-                native = T, crs = "+proj=latlong")
-aef81 <- raster(paste(inpath, "200806280945_py81_mask_tau_gt_05.rst", sep = "/"),
-                native = T, crs = "+proj=latlong")
-tau72 <- raster(paste(inpath, "200806280945_py72_mask_tau_gt_05.rst", sep = "/"),
-                native = T, crs = "+proj=latlong")
-tau82 <- raster(paste(inpath, "200806280945_py82_mask_tau_gt_05.rst", sep = "/"),
-                native = T, crs = "+proj=latlong")
 
-ext <- extent(c(4, 17, 45.75, 56))
-mm <- map('worldHires', plot = F, fill = T, col = "grey40")
+#############################
+# COT scatter
+############################
 
-lm <- raster(paste(inpath, "europe_landsea_mask.rst",
-                   sep = "/"), native = T, crs = "+proj=latlong")
-lm <- crop(lm, ext)
-lmplot <- spplot(lm, mm = mm, maxpixels = 400000, col.regions = "grey50",
-                 colorkey = F, panel = function(..., mm) {
-                   panel.levelplot(...)
-                   panel.polygon(mm$x, mm$y, col = "grey40", lwd = 0.8)
-                 })
+xyplot1 <- xyplot(dat$py72 ~ dat$cloudsat,
+                  groups = dat$py72,
+                  ylab=list("Optical thickness (M06)",cex=1.1), 
+                  xlab=list("Optical thickness (2B-TAU)",cex=1.1),
+                  scales = list(x = list(at = c(5,10,15,20,25,30,35,40,45,50,55,60)), labels = c("5","10","15","20","25","30","35","40","45","50","55","60"),cex=1.2,
+                                y = list(at = c(5,10,15,20,25,30,35,40,45,50,55,60)), labels = c("5","10","15","20","25","30","35","40","45","50","55","60"),cex=1.2),
+                  xlim=c(0,35),
+                  ylim=c(0,35),
+                  page = function(page) grid.text('a)', x = 0.015, y = 0.95),
+                  panel = function() {
+                    panel.grid(h = -1, v = -1, col="gray50",lty=2)
+                    panel.xyplot(dat$cloudsat,dat$py72,pch = 17,cex = 0.6,col = brewer.pal(6, "Greys")[5])
+                    panel.ablineq(lm(dat$py72 ~ dat$cloudsat), r.sq = TRUE, 
+                                  rot = FALSE, 
+                                  at = 0.4, 
+                                  pos = 3,
+                                  offset = 9,
+                                  cex=1.3)
+                  } )
 
-aef75 <- crop(aef75, ext)
-aef81 <- crop(aef81, ext)
-tau72 <- crop(tau72, ext)
-tau82 <- crop(tau82, ext)
 
-diffaef <- (aef81-aef75)/((aef81+aef75)/2)*100
-values(diffaef) <- ifelse(values(diffaef) < -100 | values(diffaef) > 100, 
-                          NA, values(diffaef))
-difftau <- (tau82-tau72)/((tau82+tau72)/2)*100
-values(difftau) <- ifelse(values(difftau) < -100 | values(difftau) > 100, 
-                          NA, values(difftau))
 
-yat = seq(40, 70, 2.5)
-ylabs = paste(yat, "°N", sep = "")
-xat = seq(0, 100, 2.5)
-xlabs = ifelse(xat < 0, paste(xat, "°W", sep = ""), 
-               ifelse(xat > 0, paste(xat, "°E", sep = ""), 
-                      paste(xat, "°", sep = "")))
+xyplot3 <- xyplot(dat$py82 ~ dat$cloudsat,
+                  groups = dat$py82,
+                  ylab=list("Optical thickness (SLALOM MODIS)",cex=1.1), 
+                  xlab=list("Optical thickness (2B-TAU)",cex=1.1),
+                  scales = list(x = list(at = c(5,10,15,20,25,30,35,40,45,50,55,60)), labels = c("5","10","15","20","25","30","35","40","45","50","55","60"),cex=1.2,
+                                y = list(at = c(5,10,15,20,25,30,35,40,45,50,55,60)), labels = c("5","10","15","20","25","30","35","40","45","50","55","60"),cex=1.2),
+                  xlim=c(0,35),
+                  ylim=c(0,35),
+                  page = function(page) grid.text('c)', x = 0.015, y = 0.95),
+                  panel = function() {
+                    panel.grid(h = -1, v = -1, col="gray50",lty=2)
+                    panel.xyplot(dat$cloudsat,dat$py82,pch = 15,cex = 0.6,col = brewer.pal(6, "Greys")[4])
+                    panel.ablineq(lm(dat$py82 ~ dat$cloudsat), r.sq = TRUE, 
+                                  rot = FALSE, 
+                                  at = 0.4, 
+                                  pos = 3,
+                                  offset = 9,
+                                  cex=1.3)
+                  } ) 
 
-descr <- c("a)  COT", "b)  AEF")
+#png(filename="figure_14_01.png",width=1600,height=1600,units='px',res=300,pointsize=5)
+#xyplot1
+#dev.off()
 
-aef <- spplot(diffaef, maxpixels = 400000, 
-              col.regions = colorRampPalette(c("darkblue", "aquamarine", 
-                                               "white", "orange", "darkred")),
-              at = seq(-100, 100, 0.1), panel = function(...) {
-                  panel.abline(h = yat, v = xat,
-                               col = "grey80", lwd = 0.8, lty = 3)
-                  panel.levelplot(...)
-                }, scales = list(x = list(at = xat, labels = xlabs), 
-                                 y = list(at = yat, labels = ylabs)),
-                colorkey = list(space = "top", width = 1, height = 0.75,
-                                at = seq(-100, 100, 0.1)))
+#png(filename="figure_14_03.png",width=1600,height=1600,units='px',res=300,pointsize=5)
+#xyplot3
+#dev.off()
 
-tau <- spplot(difftau, maxpixels = 400000, 
-              col.regions = colorRampPalette(c("darkblue", "aquamarine", 
-                                               "white", "orange", "darkred")),
-              at = seq(-100, 100, 0.1), panel = function(...) {
-                panel.abline(h = yat, v = xat,
-                             col = "grey80", lwd = 0.8, lty = 3)
-                panel.levelplot(...)
-              }, scales = list(x = list(at = xat, labels = xlabs), 
-                               y = list(at = yat, labels = ylabs)),
-              colorkey = list(space = "top", width = 1, height = 0.75,
-                              at = seq(-100, 100, 0.1)))
 
-out <- c(tau, aef, x.same = T, y.same = T) +
-  as.layer(lmplot, under = T)
 
-#out <- c(out, visp, x.same = T, y.same = T)
-out <- update(out, layout = c(2, 1),
-              strip = strip.custom(bg = "grey20", 
-                                   factor.levels = descr,
-                                   par.strip.text = list(
-                                     col = "white", font = 2, cex = 0.8)))
 
-png(paste(inpath, "figure_14.png", sep = "/"), res = 300, width = 1024*3, 
-    height = 768*3)
-print(out)
+#############################
+# COT percentage differences
+############################
+
+difr1 = (dat$py72-dat$cloudsat)/((dat$cloudsat+dat$py72)/2)*100
+
+xyplot2 <- xyplot(difr1 ~ dat$cloudsat, 
+          xlab=list("Optical thickness (2B-TAU)",cex=1.1), 
+          ylab=list("COT (M06)/COT (2B-TAU)  %",cex=1.1),
+          scales = list(y = list(at = c(-100,-75,-50,-25,0,25,50,75,100), labels = c("-100","-75","-50","-25","0","25","50","75","100")),cex=1.2,
+                     x = list(at = c(5,10,15,20,25,30,35,40)), labels = c("5","10","15","20","25","30","35","40"),cex=1.2),
+          xlim=c(0,35),
+          ylim=c(-100,100),
+          page = function(page) grid.text('b)', x = 0.015, y = 0.95),
+          panel = function() {
+            panel.grid(h = -1, v = -1, col="gray50",lty=2)
+            panel.xyplot(dat$cloudsat, difr1,pch=17,cex = 0.6,col=brewer.pal(6, "Greys")[5])
+            panel.abline(h=0, col = "black")
+                   }
+       ) 
+
+difr2 = ((dat$py82-dat$cloudsat)/((dat$cloudsat+dat$py82)/2))*100
+
+xyplot4 <- xyplot(difr2 ~ dat$cloudsat, 
+                  xlab=list("Optical thickness (2B-TAU)",cex=1.1), 
+                  ylab=list("COT (SLALOM MODIS)/COT (2B-TAU)  %",cex=1.1),
+                  scales = list(y = list(at = c(-100,-75,-50,-25,0,25,50,75,100), labels = c("-100","-75","-50","-25","0","25","50","75","100")),cex=1.2,
+                                x = list(at = c(5,10,15,20,25,30,35,40)), labels = c("5","10","15","20","25","30","35","40"),cex=1.2),
+                  xlim=c(0,35),
+                  ylim=c(-100,100),
+                  page = function(page) grid.text('d)', x = 0.015, y = 0.95),
+                  panel = function() {
+                    panel.grid(h = -1, v = -1, col="gray50",lty=2)
+                    panel.xyplot(dat$cloudsat, difr2,pch=15,cex = 0.6,col=brewer.pal(6, "Greys")[4])
+                    panel.abline(h=0, col = "black")
+                  }
+)
+
+#png(filename="figure_14_02.png",width=1800,height=1600,units='px',res=300,pointsize=5)
+#xyplot2
+#dev.off()
+
+#png(filename="figure_14_04.png",width=1800,height=1600,units='px',res=300,pointsize=5)
+#xyplot4
+#dev.off()
+
+### print ###
+png(paste(inpath, "figure_14.png", sep = "/"), res = 300, width = 1024*3, height = 1024*3)
+print(xyplot1, split = c(1,1,2,2), more = T)
+print(xyplot2, split = c(2,1,2,2), more = T)
+print(xyplot3, split = c(1,2,2,2), more = T)
+print(xyplot4, split = c(2,2,2,2))
 dev.off()
+
+#####################
+# statistical values
+#####################
+
+min(dat$py72)
+max(dat$py72)
+median(dat$py72)
+mean(dat$py72)
+sd(dat$py72)
+
+min(dat$py82)
+max(dat$py82)
+median(dat$py82)
+mean(dat$py82)
+sd(dat$py82)
+
+min(dat$cloudsat)
+max(dat$cloudsat)
+median(dat$cloudsat)
+mean(dat$cloudsat)
+sd(dat$cloudsat)
+
+c<-cor(dat$cloudsat,dat$py72)
+c
+c<-cor(dat$cloudsat,dat$py82)
+c
+
+
+mean_py72 <- mean(dat$cloudsat-dat$py72)
+mean_py82 <- mean(dat$cloudsat-dat$py82)
+mean_py82
+mean_py72
+rmse_py82 <- sqrt(mean((dat$cloudsat-dat$py82)^2))
+rmse_py82
+rmse_py72 <- sqrt(mean((dat$cloudsat-dat$py72)^2))
+rmse_py72
+sd(dat$cloudsat-dat$py82)
+sd(dat$cloudsat-dat$py72)
+
+
+
+#############################################################################################
+# plot
+#############################################################################################
+
+
+
+
+
+
--- figures/paper_kuehnlein_2013/figure_7.R
+++ figures/paper_kuehnlein_2013/figure_7.R
@@ -1,67 +1,219 @@
-library(raster)
+# R packages
+# install.packages("sm") # if sm isn't installed yet
+# install.packages("IDPmisc")
+library(IDPmisc)
+library(sm)
 library(latticeExtra)
-library(fields)
+library(rattle)
 
-inpath <- "/media/Meero/validation_atlantic_myd06/figures/figure_11"
 
-aef75 <- raster(paste(inpath, "b200806111415_py75_mask_tau_gt_05.rst", sep = "/"),
-                native = T, crs = "+proj=latlong")
-aef81 <- raster(paste(inpath, "c200806111415_py81_mask_tau_gt_05.rst", sep = "/"),
-                native = T, crs = "+proj=latlong")
-tau72 <- raster(paste(inpath, "b200806111415_py72_mask_tau_gt_05.rst", sep = "/"),
-                native = T, crs = "+proj=latlong")
-tau82 <- raster(paste(inpath, "c200806111415_py82_mask_tau_gt_05.rst", sep = "/"),
-                native = T, crs = "+proj=latlong")
+# read data
+filename <- file.choose()
+dat <- read.table(filename, header=T, na.strings="-99.000000")
+dat <- subset(dat,dat$aefSlalom < 30 & dat$aefMYD06 < 30)
+dat <- subset(dat,dat$aefSlalom > 5 & dat$aefMYD06 > 5)
+attach(dat)
+aefSlalom05 <- aefSlalom
+aefMYD0605 <- aefMYD06
 
-ext <- extent(c(-25, -10, 30, 50))
+###########################
+######## statistics ######
+###########################
 
-aef75 <- crop(aef75, ext)
-aef81 <- crop(aef81, ext)
-tau72 <- crop(tau72, ext)
-tau82 <- crop(tau82, ext)
+min(aefSlalom)
+max(aefSlalom)
+median(aefSlalom)
+mean(aefSlalom)
+sd(aefSlalom)
+#modalvalue(aefSlalom)
 
-diffaef <- (aef81-aef75)/((aef81+aef75)/2)*100
-values(diffaef) <- ifelse(values(diffaef) > 100, NA, values(diffaef))
-difftau <- (tau82-tau72)/((tau82+tau72)/2)*100
-values(difftau) <- ifelse(values(difftau) > 100, NA, values(difftau))
+min(aefMYD06)
+max(aefMYD06)
+median(aefMYD06)
+mean(aefMYD06)
+sd(aefMYD06)
+#modalvalue(aefMYD06)
 
-yat = seq(32.5, 47.5, 2.5)
-ylabs = paste(yat, "??N", sep = "")
-xat = seq(-22.5, -12.5, 2.5)
-xlabs = ifelse(xat < 0, paste(xat, "??W", sep = ""), 
-               ifelse(xat > 0, paste(xat, "??E", sep = ""), 
-                      paste(xat, "??", sep = "")))
+c=cor(aefSlalom,aefMYD06)
+c
 
-descr <- c("a)  AEF", "b)  COT")
 
-aef <- spplot(diffaef, maxpixels = 400000, col.regions = tim.colors(1000), 
-                cuts = 200, panel = function(...) {
-                  panel.abline(h = yat, v = xat,
-                               col = "grey30", lwd = 0.5)
-                  panel.levelplot(...)
-                }, scales = list(x = list(at = xat, labels = xlabs), 
-                                 y = list(at = yat, labels = ylabs)),
-                colorkey = list(space = "top", width = 1, height = 0.75))
+mean(aefMYD06-aefSlalom)
+sqrt(mean((aefMYD06-aefSlalom)^2))
+sd(aefMYD06-aefSlalom)
 
-tau <- spplot(difftau, maxpixels = 400000, col.regions = tim.colors(1000), 
-              cuts = 200, panel = function(...) {
-                panel.abline(h = yat, v = xat,
-                             col = "grey30", lwd = 0.5)
-                panel.levelplot(...)
-              }, scales = list(x = list(at = xat, labels = xlabs), 
-                               y = list(at = yat, labels = ylabs)),
-              colorkey = list(space = "top", width = 1, height = 0.75))
+####################################################################
+## scatter plot mit Punktdichte pro Rasterzelle, keine Interpolation um die Rasterzellen
+#####################################################################
+png(filename="200806111415_py82slalom_vs_py72_hexbin_mask_tau_gt_05.png",width=1600,height=1600,units='px',res=300,pointsize=10)
 
-out <- c(aef, tau, x.same = T, y.same = T)
+iplot(aefMYD06, aefSlalom, 
+      pixs=0.7, 
+      ztransf=function(x){x[x<1] <- 1; log2(x)}, 
+      colramp = colorRampPalette(c("grey100","grey85","grey75","grey50","grey25","grey1","black")),
+      xlab="Optical thickness (M06)", 
+      ylab="Optical thickness (SLALOM MODIS)",
+      cex.lab=1.3,
+      cex=1.1,
+      cex.axis=1,
+      legend=FALSE
+      )
 
-#out <- c(out, visp, x.same = T, y.same = T)
-out <- update(out, layout = c(2, 1),
-              strip = strip.custom(bg = "grey20", 
-                                   factor.levels = descr,
-                                   par.strip.text = list(
-                                     col = "white", font = 2, cex = 0.8)))
-
-png(paste(inpath, "figure_3.png", sep = "/"), res = 300, width = 1024*3, 
-    height = 768*3)
-print(out)
+grid(col="gray50",lty=2)
+mtext(" a)", side=2,line=3, cex=1.5, at=c(41),adj=1)
 dev.off()
+
+################################
+### percentage difference ######
+###############################
+
+dif = (aefSlalom-aefMYD06)/((aefSlalom+aefMYD06)/2)*100
+datdif <- cbind(dat,dif)
+datdif <- subset(datdif,datdif$dif < 101 & datdif$dif > -101)
+(10-30)/((10+30)/2)*100
+
+png(filename="200806111415_py82slalom_vs_py72_hexbin_error_mask_tau_gt_05.png",width=1600,height=1600,units='px',res=300,pointsize=10)
+
+iplot(datdif$aefMYD06,datdif$dif, 
+      pixs=0.7, 
+      ztransf=function(x){x[x<1] <- 1; log2(x)}, 
+      colramp = colorRampPalette(c("grey100","grey85","grey75","grey50","grey25","grey1","black")),
+      xlab="Optical thickness (M06)", 
+      ylab="COT (SLALOM MODIS)/COT(M06) %",
+      cex.lab=1.3,
+      cex=1.1,
+      cex.axis=1,
+      legend=FALSE)
+mtext(" c)", side=2,line=3, cex=1.5, at=c(106),adj=1)
+grid(col="gray50",lty=2)
+abline(h=0, col = "black")
+dev.off()
+
+
+###########################
+#### density plot  ######
+###########################
+
+png(filename="200806280945_mpy82_vs_py72_histogram_mask_tau_gt_05_2.png",width=1600,height=1600,units='px',res=300,pointsize=10)
+
+plot(density(aefSlalom,bw=0.1),
+      lwd=1.2, 
+      col="black", 
+      lty=1,
+      xlim=c(5,40), 
+      ylim=c(0,0.15),
+      xlab="Optical thickness",
+      ylab="Density",
+      cex.lab=1.3,
+      main="",
+      yaxt="n",
+      xaxt="n")
+
+axis(2,las=1,cex.axis=1.0)
+axis(1,cex.axis=1.0)
+lines(density(aefMYD06,bw=0.1), lty=2, lwd=1.2, col="black")
+#lines(density(aefSlalomA,bw=0.1), lty=3, lwd=1.2, col="black")
+
+legend("topright", 
+       inset=.05, 
+       c("COT","SLALOM","M06"),
+       lty = c(0,1,2),
+       lwd = c(0,1.2,1.2),
+       horiz=FALSE,
+       cex=1.1)
+grid(col="gray50",lty=2)
+mtext(" e)", side=2,line=3, las = 1, cex=1.5, at=c(0.151),adj=1)
+
+dev.off()
+
+## AEF ###
+####################################################################
+## scatter plot mit Punktdichte pro Rasterzelle, keine Interpolation um die Rasterzellen
+#####################################################################
+png(filename="200806111415_py81slalom_vs_py75_hexbin_mask_tau_gt_05.png",width=1600,height=1600,units='px',res=300,pointsize=10)
+
+iplot(aefMYD06, aefSlalom, 
+      pixs=0.7, 
+      ztransf=function(x){x[x<1] <- 1; log2(x)}, 
+      colramp = colorRampPalette(c("grey100","grey85","grey75","grey50","grey25","grey1","black")),
+      xlab="Effective radius, µm (M06)", 
+      ylab="Effective radius, µm (SLALOM MODIS)",
+      cex.lab=1.3,
+      cex=1.1,
+      cex.axis=1,
+      legend=FALSE
+      )
+
+grid(col="gray50",lty=2)
+mtext(" b)", side=2,line=3, cex=1.5, at=c(31),adj=1)
+dev.off()
+
+################################
+### percentage difference ######
+###############################
+
+dif = (aefSlalom-aefMYD06)/((aefSlalom+aefMYD06)/2)*100
+datdif <- cbind(dat,dif)
+datdif <- subset(datdif,datdif$dif < 101 & datdif$dif > -101)
+(10-30)/((10+30)/2)*100
+
+png(filename="200806111415_py81slalom_vs_py75_hexbin_error_mask_tau_gt_05.png",width=1600,height=1600,units='px',res=300,pointsize=10)
+
+iplot(datdif$aefMYD06,datdif$dif, 
+      pixs=0.7, 
+      ztransf=function(x){x[x<1] <- 1; log2(x)}, 
+      colramp = colorRampPalette(c("grey100","grey85","grey75","grey50","grey25","grey1","black")),
+      xlab="Effective radius, µm (M06)", 
+      ylab="AEF (SLALOM MODIS)/AEF(M06) %",
+      cex.lab=1.3,
+      cex=1.1,
+      cex.axis=1,
+      legend=FALSE)
+mtext(" d)", side=2,line=3, cex=1.5, at=c(106),adj=1)
+grid(col="gray50",lty=2)
+abline(h=0, col = "black")
+dev.off()
+
+
+###########################
+#### density plot  ######
+###########################
+
+png(filename="200806280945_mpy81_vs_py75_histogram_mask_tau_gt_05_2.png",width=1600,height=1600,units='px',res=300,pointsize=10)
+
+plot(density(aefSlalom05,bw=0.05),
+      lwd=1.8, 
+      col="black", 
+      lty=1,
+      xlim=c(0,30), 
+      ylim=c(0,0.3),
+      xlab="Effective radius, µm",
+      ylab="Density",
+      cex.lab=1.3,
+      main="",
+      yaxt="n",
+      xaxt="n")
+
+axis(2,las=1,cex.axis=1.0)
+axis(1,cex.axis=1.0)
+lines(density(aefMYD0605,bw=0.05), lty=2, lwd=1.8, col="black")
+lines(density(aefSlalom,bw=0.05), lty=1, lwd=1.0, col="black")
+lines(density(aefMYD06,bw=0.05), lty=2, lwd=1.0, col="black")
+
+legend("topright", 
+       inset=.05, 
+       c("AEF","SLALOM (COT>5)","M06 (COT>5)", "SLALOM (COT>10)", "M06 (COT>10)"),
+       lty = c(0,1,2,1,2),
+       lwd = c(0,1.8,1.8,1.0,1.0),
+       horiz=FALSE,
+       cex=1.1)
+grid(col="gray50",lty=2)
+mtext(" f)", side=2,line=3, las = 1, cex=1.5, at=c(0.31),adj=1)
+
+dev.off()
+
+
+
+
+
+
