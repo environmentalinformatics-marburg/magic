bf93bc305358497df5e354a2a1bdafa78c3ad75d
sforteva <spaska.forteva@geo.uni-marburg.de>
2013-08-19 10:16:34 +0200

    Changed the comment

---
+++ dfg_for_kilimanjaro/rename_images_by_datetime/order_by_date.py
@@ -0,0 +1,119 @@
+"""This program orders the images from the given directory by date 
+into a new order structure year-month.
+
+Copyright (C) 2013, Spaska forteva
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Please send any comments, suggestions, criticism, or (for our sake) bug
+reports to spaska.forteva@geo.uni-marburg.de
+"""
+from string import split
+from mercurial.util import copyfile
+from PIL import Image
+from PIL.ExifTags import TAGS
+import os
+import re 
+from optparse import OptionParser
+
+__author__ = "Spaska Froteva<spaska.forteva@geo.uni-marburg.de>"
+__version__ = "2013-08-08"
+__license__ = "GNU GPL, see http://www.gnu.org/licenses/"
+
+""" Read from the console the main directory 
+"""
+parser = OptionParser()
+parser.add_option('-s', '--source', 
+                  dest = "source", 
+                  default = "/home/eibestations/ei_data_kilimanjaro/processing/tic/incoming/",
+                  )
+parser.add_option('-d', '--dest', 
+                  dest = "dest", 
+                  default = "/home/eibestations/ei_data_kilimanjaro/processing/tic/processing/",
+                  )
+
+(options, args) = parser.parse_args()
+
+src = options.source
+dest = options.dest
+years = ('2011', '2012', '2013')
+months = ('01', '02', '03', '04', '05', '06', '07', '08', '09','10','11','12')
+
+"""def ensure_dirs():
+    for year in years:
+        make_sure_path_exists(dest + "" + year)
+        for month in months:
+            make_sure_path_exists(dest + "" + year + "/" + month + "_" + year)
+"""
+
+
+def make_sure_path_exists(path):
+    try:
+        if not os.path.exists(path): 
+            os.makedirs(path)
+    except OSError as exception:
+        if OSError.errno == 2:
+            pass
+        else:
+            raise      
+
+	
+def get_date(fn):
+    ret = {}
+    image = Image.open(fn)
+    from PIL.ExifTags import TAGS as id_names
+    image_tags = image._getexif()
+    creation_date = image_tags[36867]
+    
+    creation_date = split(creation_date, " ")
+    creation_date = split (creation_date[0], ':')
+    return creation_date
+   
+     
+def orderFiles(sourcePath, file):
+    srcPath = sourcePath + "/" + file
+    d = get_date(srcPath)
+    destPath = dest + d[0] + "/" + d[1] + "_" + d[0] + "/" + file
+    make_sure_path_exists(dest + d[0] + "/" + d[1] + "_" + d[0])
+    if not (os.path.exists(destPath)):
+        copyfile(srcPath, destPath)
+             
+def main():
+    '''Call the file rename recursively
+
+    Args:
+        src: Source directory for the recursive search
+    '''
+    try:
+        
+        for sourcePath, dirs, files in os.walk(src):
+            if len(files) > 0:
+                for file in files:
+                    orderFiles(sourcePath,file)
+            #if len(meinDirs) > 0:
+            #    for dir in meinDirs:
+            #        orderFiles(mainRoot + dir)
+            
+                
+    except Exception as inst:
+        print "An error occured with the following dataset."
+        print "Some details:"
+        print "Exception type: " , type(inst)
+        print "Exception args: " , inst.args
+        print "Exception content: " , inst 
+    
+    
+main()
+    
+    
