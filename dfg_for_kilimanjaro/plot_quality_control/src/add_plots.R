eff957b3ad8212c1e7dde5b17a830aa6c106d542
tnauss <nausst@gmail.com>
2014-04-24 12:14:03 +0200

    Update

--- dfg_for_kilimanjaro/plot_quality_control/src/add_plots.R
+++ dfg_for_kilimanjaro/plot_quality_control/src/add_plots.R
@@ -2,46 +2,25 @@
 library(rgdal)
 library(raster)
 
-# Functions for dX and xY to move Whittaker plot with respect to other poles
-set.dXW <- function(df, mv, pos){
-  abs(df[df$PlotID == as.character(mv$PlotID[pos]) & 
-           df$PoleName == "A middle pole", ]$coords.x1 - 
-        df[df$PlotID == as.character(mv$PlotID[pos]) & 
-             df$PoleName == "B7", ]$coords.x1 - 
-        df[df$PlotID == as.character(mv$PlotID[pos]) & 
-             df$PoleName == "A1", ]$coords.x1 + 
-        df[df$PlotID == as.character(mv$PlotID[pos]) & 
-             df$PoleName == "A3", ]$coords.x1) *
-    mv$fX[pos] + 4.8211
-}
-
-set.dYW <- function(df, mv, pos){
-  abs(df[df$PlotID == as.character(mv$PlotID[pos]) & 
-           df$PoleName == "A middle pole", ]$coords.x2 - 
-        df[df$PlotID == as.character(mv$PlotID[pos]) & 
-             df$PoleName == "B7", ]$coords.x2) *
-    mv$fY[pos]
-}
-
 # Working directory and data set
 dsn <- switch(Sys.info()[["sysname"]], 
               "Linux" = "/media/permanent/",
               "Windows" = "D:/")
 setwd(paste0(dsn, "active/kilimanjaro_plot_quality_control/data"))
-input.filepath <- "plot_poles_arc1960_mod/PlotPoles_ARC1960_mod_20140410_v04_add_by_bing.shp"
+input.filepath <- "plot_poles_arc1960_mod/PlotPoles_ARC1960_mod_20140410_v04_added_by_bing.shp"
 output.path <- paste(dirname(input.filepath), "moved", sep = "/")
 output.filepath <- paste(output.path, basename(input.filepath), sep = "/")
 layer <- sub("^([^.]*).*", "\\1", basename(input.filepath))
 
 # Set new plot details
-plots.new <- data.frame(PlotID = c("emg0", "mch0", "foc0", "mwh0", "mai0", "sav0", "foc6", "mcg0", "fpo0"),
+plots.new <- data.frame(PlotID = c("emg0", "mch0", "foc0", "mwh0", "mai0", "sav0", "foc6", "mcg0", "fpo0", "fpd0"),
                         PoleName = "A middle pole",
                         PoleType = "AMP",
                         POINT_X = NaN,
                         POINT_Y = NaN,
                         coords_x3 = 0,
-                        coords.x1 = c(335386, 307231, 305477, 318531, 304694, 353957, 304864, 304864, 306464),
-                        coords.x2 = c(9641965, 9658017, 9655113, 9651408, 9634264, 9634163, 9652552, 9652552, 9657059))
+                        coords.x1 = c(335386, 307231, 305477, 318531, 304694, 353957, 304864, 304864, 306464, 318544),
+                        coords.x2 = c(9641965, 9658017, 9655113, 9651408, 9634264, 9634163, 9652552, 9652552, 9657059, 9650477))
 
 
 # Amend dataset and write to shape file
