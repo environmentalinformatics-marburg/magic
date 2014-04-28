d3c5e5ed38cf0be2f621f5a34752e66f820e3b81
fdetsch <flowla88@gmail.com>
2014-04-28 10:29:37 +0200

    Update: minor performance improvement to buffering script.

--- dfg_for_kilimanjaro/kilimanjaro_lcp/extractLandcover/src/lcpBufferExtraction.R
+++ dfg_for_kilimanjaro/kilimanjaro_lcp/extractLandcover/src/lcpBufferExtraction.R
@@ -41,15 +41,20 @@ rst.lcp <- lapply(fls.lcp, raster)
 # Extract class ids (column 'Value' in each raster's attribute table) and 
 # related class names (column 'Class_name')
 rst.lcp.id <- do.call("rbind", lapply(rst.lcp, function(i) {
-  data.frame(i@data@attributes[[1]]$Value, tolower(i@data@attributes[[1]]$Class_name), 
+  data.frame(i@data@attributes[[1]]$Value, 
+             tolower(i@data@attributes[[1]]$Class_name), 
              stringsAsFactors = FALSE)
 }))
 names(rst.lcp.id) <- c("luv", "luc")
 # Remove duplicated rows
-rst.lcp.id <- rst.lcp.id[!duplicated(rst.lcp.id),]
+rst.lcp.id <- rst.lcp.id[!duplicated(rst.lcp.id), ]
 # Check for data inconsistencies
 if (anyDuplicated(rst.lcp.id[, 1]) > 0)
-  stop(paste("found duplicated class id:", rst.lcp.id[duplicated(rst.lcp.id[,1]),1]))
+  stop(paste("found duplicated class id:", rst.lcp.id[duplicated(rst.lcp.id[, 1]), 1]))
+
+# Identify class values for class names 'clouds' and 'shadow'
+id.cl <- rst.lcp.id[rst.lcp.id$luc == "clouds", 1]
+id.sh <- rst.lcp.id[rst.lcp.id$luc == "shadow", 1]
 
 # Merge classified images
 rst.lcp.mrg <- do.call(function(...) merge(..., tolerance = 1), rst.lcp)
@@ -63,9 +68,10 @@ rst.lcp.mrg <- do.call(function(...) merge(..., tolerance = 1), rst.lcp)
 plot.names <- unique(as.character(shp@data$PlotID))
 
 # Land-use classes -> column names
-class.names <- sort(as.character(rst.lcp.id$luc))
+class.names <- sort(rst.lcp.id$luc)
 
-output <- data.frame(matrix(ncol = 1 + length(class.names), nrow = length(plot.names)))
+output <- data.frame(matrix(ncol = 1 + length(class.names), 
+                            nrow = length(plot.names)))
 output[] <- 0
 names(output) <- c("PlotID", class.names)
 
@@ -91,24 +97,27 @@ buffered.values <- lapply(1:nrow(shp), function(i) {
     if (sum(!is.na(tmp.bff.val)) > 0) {
       # Calculate and round percentage of each class within buffer
       tmp.df <- as.data.frame(round(table(tmp.bff.val) / length(tmp.bff.val), 
-                                    digits = 3), stringsAsFactors = FALSE)
+                                 digits = 3), stringsAsFactors = FALSE)
+      # Replace class values with corresponding class names
       tmp.df[, 1] <- rst.lcp.id[sapply(1:nrow(tmp.df), function(k) {
         which(rst.lcp.id == tmp.df[k, 1])
-        }), 2]
+      }), 2]
       
       # Remove clouds and shadows
-      id.cl <- rst.lcp.id[rst.lcp.id$luc == "clouds", 1]
-      id.sh <- rst.lcp.id[rst.lcp.id$luc == "shadow", 1]
-      clsh <- which(tmp.bff.val %in% c(id.cl, id.sh))
+      cf <- which(tmp.bff.val %in% c(id.cl, id.sh))
       
-      if (length(clsh) > 0) {
-        tmp.bff.val.rm_clsh <- tmp.bff.val[-clsh]
-        tmp.df.rm_clsh <- as.data.frame(round(table(tmp.bff.val.rm_clsh) / length(tmp.bff.val.rm_clsh), digits = 3), stringsAsFactors = FALSE)
-        tmp.df.rm_clsh[, 1] <- rst.lcp.id[sapply(1:nrow(tmp.df.rm_clsh), function(k) which(rst.lcp.id == tmp.df.rm_clsh[k, 1])), 2]
-        tmp <- merge(tmp.df, tmp.df.rm_clsh, by.x = names(tmp.df)[1], by.y = names(tmp.df.rm_clsh)[1], all = TRUE)
+      if (length(cf) > 0) {
+        tmp.bff.val.cf <- tmp.bff.val[-cf]
+        tmp.tbl.cf <- round(table(tmp.bff.val.cf) / length(tmp.bff.val.cf), 3)
+        tmp.df.cf <- as.data.frame(tmp.tbl.cf, stringsAsFactors = FALSE)
+        tmp.df.cf[, 1] <- rst.lcp.id[sapply(1:nrow(tmp.df.cf), function(k) {
+          which(rst.lcp.id == tmp.df.cf[k, 1])
+        }), 2]
+        tmp <- merge(tmp.df, tmp.df.cf, by.x = names(tmp.df)[1], 
+                     by.y = names(tmp.df.cf)[1], all = TRUE)
       } else {
-        tmp.df.rm.clsh <- tmp.df
-        tmp <- data.frame(tmp.df, tmp.df.rm.clsh[, 2])
+        tmp.df.rm.cf <- tmp.df
+        tmp <- data.frame(tmp.df, tmp.df.rm.cf[, 2])
       }
       
       # Merge and return data
@@ -128,7 +137,7 @@ buffered.values <- lapply(1:nrow(shp), function(i) {
 out <- lapply(seq(buffers), function(z) {
   # Duplicate output template
   tmp.out <- output
-  tmp.out.rm_clsh <- output
+  tmp.out.cf <- output
   # Extract values from current buffer column
   tmp.val <- sapply(buffered.values, "[", z)
   
@@ -139,23 +148,24 @@ out <- lapply(seq(buffers), function(z) {
       tmp.out[i, 2:ncol(tmp.out)] <- NA
     } else {
       tmp.out[i, tmp.val[[i]][, 1]] <- tmp.val[[i]][, 2]   
-      tmp.out.rm_clsh[i, tmp.val[[i]][, 1]] <- tmp.val[[i]][, 3] 
+      tmp.out.cf[i, tmp.val[[i]][, 1]] <- tmp.val[[i]][, 3] 
     }
   }
   
   # Write CSV and return output data
-  write.csv(tmp.out, paste0("out/kili_lcp_", buffers[z], "m.csv"), row.names = FALSE)
-  write.csv(tmp.out.rm_clsh, paste0("out/kili_lcp_", buffers[z], "m_rm_clsh.csv"), row.names = FALSE)
-  return(list(tmp.out, tmp.out.rm_clsh))
+  write.csv(tmp.out, paste0("out/kili_lcp_", buffers[z], "m.csv"), 
+            row.names = FALSE)
+  write.csv(tmp.out.cf, paste0("out/kili_lcp_", buffers[z], "m_cf.csv"), 
+            row.names = FALSE)
+  return(list(tmp.out, tmp.out.cf))
 })
 
 # Concatenate output tables to one large table
 out.final <- do.call("cbind", lapply(seq(buffers), function(i) {
   do.call(function(...) {
-    merge(..., by = "PlotID", suffixes = c(paste0("_", buffers[i]), 
-                                           paste0("_", buffers[i], "_nc")), 
-          all = TRUE), out[[i]]
-    })}))
+    merge(..., by = "PlotID", all = TRUE,
+          suffixes = c(paste0("_", buffers[i]), paste0("_", buffers[i], "_nc")))
+    }, out[[i]])}))
 
 out.final <- out.final[!duplicated(colnames(out.final))]
 write.csv(out.final, "out/kili_lcp_allbuff.csv", row.names = FALSE, quote = FALSE)
