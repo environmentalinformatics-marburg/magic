4729ad27de941ca02636ac4f1e7c43bc81a8e047
Hanna <hanna.meyer1@gmail.com>
2014-05-02 16:37:31 +0200

    some extensions...

--- comparison_machineLearning/subscripts/VisualizationOfModelPrediction.R
+++ comparison_machineLearning/subscripts/VisualizationOfModelPrediction.R
@@ -5,7 +5,9 @@ load(paste(resultpath,"/prediction_nnet.RData",sep=""))
 load(paste(resultpath,"/prediction_svm.RData",sep=""))
 load(paste(resultpath,"/predictorVariables.RData",sep=""))
 
-
+##################################################################################################################
+#                                         ROC
+##################################################################################################################
 obs=as.character(prediction_svm$observed)
 obs[obs=="rain"]=1
 obs[obs=="norain"]=0
@@ -31,6 +33,20 @@ legend("bottomright",col=c("black","red","blue","green","grey"),lty=c(1),
                                                      paste("NNET",round(rocNNET[[1]],2)),paste("SVM",round(rocSVM[[1]],2)),
                 "Random 0.5"),bty="n")
 dev.off()
+##################################################################################################################
+#                                                 CROSSTABS
+##################################################################################################################
+write.csv(table(prediction_mlp$observed,prediction_mlp$prediction),file=paste(resultpath,"/crosstabMLP.csv",sep=""))
+write.csv(table(prediction_rf$observed,prediction_rf$prediction),file=paste(resultpath,"/crosstabRF.csv",sep=""))
+write.csv(table(prediction_nnet$observed,prediction_nnet$prediction),file=paste(resultpath,"/crosstabNNET.csv",sep=""))
+write.csv(table(prediction_svm$observed,prediction_svm$prediction),file=paste(resultpath,"/crosstabSVM.csv",sep=""))
 
+pdf(paste(resultpath,"/prediction_crosstab.pdf",sep=""))
+par(mfrow=c(2,2))
+plot(prediction_rf$observed,prediction_rf$prediction,main="RF",xlab="observed",ylab="predicted")
+plot(prediction_mlp$observed,prediction_mlp$prediction,main="MLP",xlab="observed",ylab="predicted")
+plot(prediction_nnet$observed,prediction_nnet$prediction,main="NNET",xlab="observed",ylab="predicted")
+plot(prediction_svm$observed,prediction_svm$prediction,main="SVM",xlab="observed",ylab="predicted")
+dev.off()
 
-#plotObsVsPred or plotClassProbs. 
+#plotObsVsPred or plotClassProbs ??
