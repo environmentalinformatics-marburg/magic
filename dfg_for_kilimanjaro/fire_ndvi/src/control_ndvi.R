9fc8c15a2117bba1b98bf78461684f3c6ee1a37f
fdetsch <florian.detsch@staff.uni-marburg.de>
2014-10-28 15:50:11 +0100

    Update: implemented Terra vs. Aqua plotting routine.

--- dfg_for_kilimanjaro/fire_ndvi/src/control_ndvi.R
+++ dfg_for_kilimanjaro/fire_ndvi/src/control_ndvi.R
@@ -1,18 +1,40 @@
-lib <- c("raster", "doParallel")
+lib <- c("raster", "rgdal", "doParallel", "reshape2", "ggplot2", "scales")
 sapply(lib, function(x) library(x, character.only = TRUE))
 
 registerDoParallel(cl <- makeCluster(2))
 
 source("src/visMannKendall.R")
+source("../gimms3g/gimms3g/sortByElevation.R")
 
 ### DEM
 dem <- raster("data/DEM_ARC1960_30m_Hemp.tif")
+
+### KiLi plots
+plots <- readOGR(dsn = "data/coords/", 
+                 layer = "PlotPoles_ARC1960_mod_20140807_final")
+plots <- subset(plots, PoleType == "AMP")
+
+official_plots <- c(paste0("cof", 1:5), 
+                    paste0("fed", 1:5),
+                    paste0("fer", 0:4), 
+                    paste0("flm", c(1:4, 6)), 
+                    paste0("foc", 1:5), 
+                    paste0("fod", 1:5), 
+                    paste0("fpd", 1:5), 
+                    paste0("fpo", 1:5), 
+                    paste0("gra", c(1:2, 4:6)), 
+                    paste0("hel", 1:5), 
+                    paste0("hom", 1:5), 
+                    paste0("mai", 1:5), 
+                    paste0("sav", 1:5))
+
+plots <- subset(plots, PlotID %in% official_plots)
 
 ### NDVI (2003-2013)
 st_year <- "2003"
 nd_year <- "2013"
 
-foreach(i = c("mod13q1", "myd13q1"), .packages = lib, 
+rst <- foreach(i = c("mod13q1", "myd13q1"), .packages = lib, 
                   .export = "visMannKendall") %dopar% {
 
   fls_ndvi <- list.files(paste0("data/processed/whittaker_", i), 
@@ -34,5 +56,45 @@ foreach(i = c("mod13q1", "myd13q1"), .packages = lib,
                       format = "GTiff", overwrite = TRUE))
   dev.off()
   
-  system(paste("convert -trim", png_out, png_out))
+  return(rst_ndvi)
 }
+
+### NDVI MOD vs. MYD: plot basis
+ls_val <- foreach(i = rst, j = list("mod14a1", "myd14a1"), .packages = lib, 
+                  .export = "sortByElevation") %dopar% {
+  mat_val <- extract(i, plots)
+  df_val <- data.frame(PlotID = plots@data$PlotID, mat_val)
+  names(df_val)[2:ncol(df_val)] <- substr(names(df_val)[2:ncol(df_val)], 5, 11)
+  df_val <- sortByElevation(plot_names = official_plots, plot_shape = plots, 
+                            val = df_val)
+  mlt_val <- melt(df_val, id.vars = c(1, ncol(df_val)), variable.name = "date", value.name = toupper(j))
+  mlt_val$date <- as.Date(mlt_val$date, format = "%Y%j")
+  mlt_val[, toupper(j)] <- mlt_val[, toupper(j)] / 10000
+  return(mlt_val)
+}
+
+png("out/plots/ndvi_terra_vs_aqua.png", width = 24, height = 27, units = "cm", 
+    res = 300, pointsize = 15)
+ggplot() + 
+  geom_line(aes(x = date, y = MOD14A1), data = ls_val[[1]], color = "black", 
+            alpha = .35) + 
+  geom_line(aes(x = date, y = MYD14A1), data = ls_val[[2]], color = "grey", 
+            alpha = .35) + 
+  stat_smooth(aes(x = date, y = MOD14A1), data = ls_val[[1]], method = "lm", 
+              color = "black", se = FALSE, lwd = 1, lty = 1) + 
+  stat_smooth(aes(x = date, y = MYD14A1), data = ls_val[[2]], method = "lm", 
+              color = "grey", se = FALSE, lwd = 1, lty = 1) + 
+  facet_wrap(~ PlotID, ncol = 5, scales = "free_y") + 
+  scale_x_date(labels = date_format("%Y"), 
+               breaks = date_breaks(width = "4 years"), 
+               minor_breaks = waiver()) +
+  labs(x = "Time", y = "NDVI") + 
+  theme_bw() + 
+  theme(panel.grid = element_blank())
+dev.off()
+
+# Apply `convert -trim` to all png images 
+fls_png <- list.files("out/", pattern = ".png", full.names = TRUE, recursive = TRUE)
+for (i in fls_png)
+  system(paste("convert -trim", i, i))
+
--- dfg_for_kilimanjaro/fire_ndvi/src/kifiPlotting.R
+++ dfg_for_kilimanjaro/fire_ndvi/src/kifiPlotting.R
@@ -6,16 +6,16 @@ rm(list = ls(all = TRUE))
 # Working directory
 switch(Sys.info()[["sysname"]], 
        "Linux" = {path.wd <- "/media/pa_NDown/ki_modis_ndvi/"}, 
-       "Windows" = {path.wd <- "D:/modiscdata_mod/data_rst/"})
+       "Windows" = {path.wd <- "G:/ki_modis_ndvi"})
 setwd(path.wd)
 
 # Required packages and functions
 lib <- c("doParallel", "raster", "rgdal", "Kendall", "matrixStats", "ggplot2", 
-         "kza", "RColorBrewer", "zoo")
-sapply(lib, function(x) stopifnot(require(x, character.only = T)))
+         "kza", "RColorBrewer", "zoo", "scales", "latticeExtra", "grid")
+sapply(lib, function(x) stopifnot(require(x, character.only = TRUE)))
 
 # Parallelization
-registerDoParallel(cl <- makeCluster(4))
+registerDoParallel(cl <- makeCluster(3))
 
 
 ### Data import
@@ -23,18 +23,20 @@ registerDoParallel(cl <- makeCluster(4))
 ## MODIS NDVI 
 
 # List files and order by date
-ndvi.fls <- list.files("kifi",  
-                       recursive = F, full.names = T)
+# ndvi.fls <- list.files("data/gap_filled/", pattern = "MYD13Q1", 
+ndvi.fls <- list.files("data/processed/whittaker_mod13q1", 
+                       pattern = "^WHT.*.tif$", full.names = TRUE)
 
-ndvi.dates <- substr(basename(ndvi.fls), 16, 22)
-ndvi.years <- unique(substr(basename(ndvi.fls), 16, 19))
+ndvi.dates <- substr(basename(ndvi.fls), 5, 11)
+ndvi.years <- unique(substr(basename(ndvi.fls), 5, 11))
 
-ndvi.fls <- ndvi.fls[order(ndvi.dates)]
+## Notwendig?
+## ndvi.fls <- ndvi.fls[order(ndvi.dates)]
 
 # Setup time series
 ndvi.ts <- do.call("c", lapply(ndvi.years, function(i) { 
   seq(as.Date(paste(i, "01", "01", sep = "-")), 
-      as.Date(paste(i, "12", "31", sep = "-")), 1)
+      as.Date(paste(i, "12", "31", sep = "-")), 16)
 }))
 
 # Merge time series with available NDVI files
@@ -48,15 +50,12 @@ ndvi.rst <- foreach(i = seq(nrow(ndvi.ts.fls)), .packages = lib) %dopar% {
   if (is.na(ndvi.ts.fls[i, 2])) {
     NA
   } else {
-    raster(ndvi.ts.fls[i, 2])
+    round(raster(ndvi.ts.fls[i, 2]) / 10000, 2)
   }
 }
 
-head(ndvi.ts.fls)
-
 # Remove whitespace
-
-ndvi.shp <- rasterToPolygons(ndvi.rst[[4178]])
+ndvi.shp <- rasterToPolygons(ndvi.rst[[20]])
 
 ndvi.rst <- foreach(i = ndvi.rst, .packages = lib) %dopar% {
   if (class(i) == "logical") NA else crop(i, extent(ndvi.shp))
@@ -74,13 +73,16 @@ plot.lonlat <- read.csv("kili_plots_64.csv",
                         stringsAsFactors = F)[, c(1, 2, 6, 13, 14)]
 plot.lonlat <- plot.lonlat[complete.cases(plot.lonlat), ]
 plot.lonlat <- subset(plot.lonlat, Valid == "Y")
-plot.lonlat <- plot.lonlat[!substr(plot.lonlat$PlotID, 1, 3) %in% c("jul", "sun"),]
+plot.lonlat <- plot.lonlat[!substr(plot.lonlat$PlotID, 1, 3) %in% c("jul", "sun"), ]
 coordinates(plot.lonlat) <- ~ POINT_X + POINT_Y
 projection(plot.lonlat) <- "+init=epsg:4326"
 plot.utm37s <- spTransform(plot.lonlat, CRS("+init=epsg:32737"))
 
 
 ## Mann-Kendall trend
+
+# DEM 
+dem <- raster("data/KiLi_DEM/kili_dem_utm.tif")
 
 # Identify first and last valid raster
 first.valid.index <- which(!sapply(ndvi.rst, is.logical))[1]
@@ -103,65 +105,58 @@ last.valid.index <- rev(which(!sapply(ndvi.rst, is.logical)))[1]
 # }, filename = "out/plots/mk_sig_myd13q1", format = "GTiff", overwrite = T)
 
 # Index of first and last scene
-ndvi.aug02 <- which(as.yearmon(ndvi.ts.fls[, 1]) == "Aug 2002")[1]
+ndvi.aug02 <- which(as.yearmon(ndvi.ts.fls[, 1]) == "Aug 2000")[1]
 ndvi.jul13 <- rev(which(as.yearmon(ndvi.ts.fls[, 1]) == "Jul 2013"))[1]
 
 # Mann-Kendall tau on 13-month running mean from Aug 2002 to Jul 2013
-ndvi.rst <- foreach(i = ndvi.rst, .packages = lib) %dopar% {
-  if (class(i) == "logical") {
-    return(NA)
-  } else {
-    i[which(i[] < 1)] <- 0
-    return(i)
-  }
-}
+ndvi.stck <- stack(ndvi.rst[ndvi.aug02:ndvi.jul13])
 
-ndvi.rst.cc <- ndvi.rst[!sapply(ndvi.rst, is.logical)]
+# ndvi.mk <- overlay(ndvi.stck, 
+#                    fun = function(x) {
+#                      tmp.kz <- kza(as.numeric(x), m = 13 * 30 / 16, k = 3, 
+#                                    impute_tails = T)$kz
+#                      MannKendall(tmp.kz)$tau
+#                    }, filename = "out/plots/mk_mod13q1_aug02_jul13", 
+#                    format = "GTiff", overwrite = T)
+# 
+# # Mann-Kendall tau of significant pixels only
+# ndvi.mk.sig <- overlay(ndvi.stck, 
+#                        fun = function(x) {
+#                          tmp.kz <- kza(as.numeric(x), m = 13 * 30 / 16, k = 3,
+#                                        impute_tails = T)$kz
+#                          mk <- MannKendall(tmp.kz)
+#                          if (mk$sl >= .001) return(NA) else return(mk$tau)
+#                        }, filename = "out/plots/mk_sig001_mod13q1_aug02_jul13", 
+#                        format = "GTiff", overwrite = T)
 
-ndvi.stck <- stack(ndvi.rst.cc)
+# # Plotting
+ndvi.mk <- raster("out/plots/mk_myd13q1_aug02_jul13.tif")
+ndvi.mk.sig <- raster("out/plots/mk_sig001_myd13q1_aug02_jul13.tif")
 
-ndvi.mk <- overlay(ndvi.stck, 
-                   fun = function(x) {
-                     tmp.kz <- kza(as.numeric(x), m = 13 * 30 /16, k = 3, 
-                                   impute_tails = T)$kz
-                     MannKendall(tmp.kz)$tau
-                   }, filename = "kifi_mk", 
-                   format = "GTiff", overwrite = T)
-
-
-# Mann-Kendall tau of significant pixels only
-ndvi.mk.sig <- foreach(z = c(.05, .01, .001), y = c("05", "01", "001"), 
-                       .packages = lib) %dopar% {
-  overlay(ndvi.stck, 
-          fun = function(x) {
-            tmp.kz <- kza(as.numeric(x), m = 13 * 30 /16, k = 3,
-                          impute_tails = T)$kz
-            mk <- MannKendall(tmp.kz)
-            if (mk$sl >= z) return(NA) else return(mk$tau)
-          }, filename = paste0("kifi_mk_sig", y), 
-          format = "GTiff", overwrite = T)
-}
-
-# Plotting
-dem <- raster("kili_dem_utm.tif")
-
-png("mk_sig001_mod06_jan02_jul13.png", units = "mm", width = 300, 
-    res = 300, pointsize = 20)
-spplot(ndvi.mk.sig[[3]], scales = list(draw = T), xlab = "x", ylab = "y", 
-       col.regions = colorRampPalette(brewer.pal(11, "BrBG")), 
-       sp.layout = list("sp.lines", rasterToContour(dem)), 
-       par.settings = list(fontsize = list(text = 15)))
-dev.off()
+# png("out/plots/mk_myd13q1_aug02_jul13.png", units = "mm", width = 300, 
+#     res = 300, pointsize = 20)
+# spplot(ndvi.mk, scales = list(draw = T), xlab = "x", ylab = "y", 
+#        col.regions = colorRampPalette(brewer.pal(11, "BrBG")), 
+#        sp.layout = list("sp.lines", rasterToContour(dem)), 
+#        par.settings = list(fontsize = list(text = 15)))
+# dev.off()
+# 
+# png("out/plots/mk_sig001_mod13q1_aug00_jul13.png", units = "mm", width = 300, 
+#     res = 300, pointsize = 20)
+# spplot(ndvi.mk.sig, scales = list(draw = T), xlab = "x", ylab = "y", 
+#        col.regions = colorRampPalette(brewer.pal(11, "BrBG")), 
+#        sp.layout = list("sp.lines", rasterToContour(dem)), 
+#        par.settings = list(fontsize = list(text = 15))) # + 
+# #   layer(sp.points(plot.utm37s[grep("foc", plot.utm37s$PlotID), 1], 
+# #                   pch = 21, col = "black", cex = 2, fill = "red"))
+# dev.off()
 
 
 ## NDVI 13 months moving average (KZA)
 
-# Rasters to matrices
-ndvi.mat <- foreach(i = ndvi.rst, .packages = lib) %dopar% as.matrix(i)
-
 # Available landuse classes
 luc <- unique(plot.utm37s$Categories)
-luc <- luc[!luc %in% c("scientific garden", "Nkweseko garden")]
+# luc <- luc[!luc %in% c("scientific garden", "Nkweseko garden")]
 
 # Loop through single landuse classes
 ndvi.lu_md <- foreach(i = luc, .packages = lib, .combine = function(...) {
@@ -172,16 +167,16 @@ ndvi.lu_md <- foreach(i = luc, .packages = lib, .combine = function(...) {
     # Extract NDVI time series per plot and calculate median NDVI per time step
     # In case of one plot per landuse class (e.g. Nkweseko)...
     if (nrow(tmp.df) > 1) {
-      data.frame(date = ndvi.ts.fls[177:4195, 1], 
+      data.frame(date = ndvi.ts.fls[12:268, 1], 
                  type = i, 
-                 value = rowMedians(foreach(j = cellFromXY(ndvi.rst[[193]], tmp.df), 
-                                            .combine = "cbind") %do% sapply(ndvi.mat[177:4195], function(l) t(l)[j]), 
-                                    na.rm = T), stringsAsFactors = F)
+                 value = rowMedians(foreach(j = cellFromXY(ndvi.rst[[20]], tmp.df), 
+                                            .combine = "cbind") %do% sapply(ndvi.mat[12:268], function(l) t(l)[j])
+                 ), stringsAsFactors = F)
       # In case of multiple plots per landuse class...
     } else {
-      data.frame(date = ndvi.ts.fls[177:4195, 1], 
+      data.frame(date = ndvi.ts.fls[12:268, 1], 
                  type = i, 
-                 value = sapply(ndvi.mat[177:4195], function(l) 
+                 value = sapply(ndvi.mat[12:268], function(l) 
                    t(l)[cellFromXY(ndvi.rst[[1]], tmp.df)]
                  ), stringsAsFactors = F)
     }
@@ -195,12 +190,6 @@ ndvi.lu_md <- foreach(i = luc, .combine = "rbind") %do% {
   return(tmp.df)
 }
 
-# # Plotting
-# ggplot(ndvi.lu_md, aes(x = date, y = value)) + 
-#   geom_line(color = "grey80") + 
-#   geom_line(aes(date, kz), color = "blue") +
-#   facet_wrap(~ type, nrow = 4, ncol = 3)
-
 
 ## Boxplot 16-day time series
 
@@ -211,11 +200,11 @@ ndvi.lu_bp <- foreach(i = luc, .packages = lib, .combine = "rbind") %dopar% {
   
   # Extract NDVI time series per plot of current landuse class
   foreach(j = seq(nrow(tmp.df)), .combine = "rbind") %do% {
-    data.frame(date = ndvi.ts.fls[177:4195, 1], 
+    data.frame(date = ndvi.ts.fls[12:268, 1], 
                plot = tmp.df$PlotID[j], 
                type = i,
-               value = sapply(ndvi.mat[177:4195], function(k) {
-                 t(k)[cellFromXY(ndvi.rst[[193]], tmp.df[j, ])]
+               value = sapply(ndvi.mat[12:268], function(k) {
+                 t(k)[cellFromXY(ndvi.rst[[20]], tmp.df[j, ])]
                }), stringsAsFactors = T)
   } 
 }
@@ -232,13 +221,6 @@ ndvi.lu_bp$season[ndvi.lu_bp$month %in% c("Oct", "Nov", "Dec")] <- "OND"
 ndvi.lu_bp$season <- as.factor(ndvi.lu_bp$season)
 levels(ndvi.lu_bp$season)
 ndvi.lu_bp$season <- factor(ndvi.lu_bp$season, levels = c("JF", "MAM", "JJAS", "OND"))
-
-# # Presentation stuff
-# ndvi.lu_md$kz <- ndvi.lu_md$kz / 10000
-# ndvi.lu_bp$value <- ndvi.lu_bp$value / 10000
-
-# ndvi.lu_md$type <- capitalize(ndvi.lu_md$type)
-# levels(ndvi.lu_bp$type) <- "Savanna"
 
 ndvi.lu_md$type <- factor(ndvi.lu_md$type)
 levels(ndvi.lu_md$type) <- c("Coffee plantation", "Forest Ericaceous", 
@@ -258,8 +240,29 @@ insert_minor <- function(major_labs, n_minor) {
   labs[1:(length(labs) - n_minor)]
 }
 
+# Import data from MODIS cloud product
+load("/media/480F-EA7D/modis_cloud/modis_cloud_agg.RData")
 
+cld.lu <- unique(cloud.lu_bp$type)
+
+cld.lu.yr <- foreach(h = cld.lu, .combine = "rbind") %do% {
+  tmp.lu <- cloud.lu_bp[grep(h, cloud.lu_bp$type), ]
+  tmp.lu.cc <- tmp.lu[complete.cases(tmp.lu), ]
+  
+  tmp.lu.yrs <- unique(substr(tmp.lu.cc$date, 1, 4))
+  
+  data.frame(date = tmp.lu.yrs, type = h, 
+             value = foreach(i = tmp.lu.yrs, .combine = "c") %do% {
+    tmp.lu.cc.yr <- tmp.lu.cc[grep(i, substr(tmp.lu.cc$date, 1, 4)), ]
+    return(round(sum(tmp.lu.cc.yr$value) / (length(tmp.lu.cc.yr$value) * 16), 2))
+  })
+}
+
+# Plot annual cloud cover percentage along with NDVI boxplot time series per
+# land-use class and corresponding 13-month moving average mean NDVI
 ggplot() + 
+  geom_histogram(aes(x = as.Date(paste0(cld.lu.yr$date, "001"), format = "%Y%j"), 
+                     y = value), data = cld.lu.yr, stat = "identity", fill = "grey60") +
   geom_boxplot(data = ndvi.lu_bp, aes(x = date, y = value, group = date, 
                                       fill = season), range = 0, 
                colour = "transparent", outlier.shape = "*") + 
@@ -271,100 +274,20 @@ ggplot() +
                 group = plot), data = ndvi.lu_bp, color = "grey70", alpha = .5) +
   geom_line(aes(x = date, y = kz), data = ndvi.lu_md, color = "red") + 
   xlab("Time (16-day intervals)") + 
-  ylab("Cloud Cover") + 
+  ylab("NDVI / Annual cloud cover [%]") + 
   theme_bw() + 
   theme(axis.title.y = element_text(size = rel(1.15), angle = 90), 
         axis.title.x = element_text(size = rel(1.15), angle = 0), 
-        strip.text.x = element_text(size = rel(0.95), angle = 0)) 
-        ggsave(filename = "ndvi_boxp_ts3.png", units = "mm", 
-               height = 300, dpi = 600)
-        
-        
-        
-
-ggplot() +
-  geom_boxplot(data = ndvi.lu_bp, aes(x = date, y = value, group = date,
-                                      fill = season), range = 0,
-               colour = "transparent", outlier.shape = "*") +
-  facet_wrap(~ type, nrow = 6, ncol = 2) +
-  scale_x_date(breaks = "2 years", minor_breaks = "1 year",
-               labels = date_format(format = "%Y")) +
-  scale_fill_manual(name = "Season", values = colors, guide = F) +
-  geom_line(aes(x = date, y = kza(value, m = 13 * 30 / 16, k = 3, impute_tails = T)$kz,
-                group = plot), data = ndvi.lu_bp, color = "grey70", alpha = .5) +
-  geom_line(aes(x = date, y = kz), data = ndvi.lu_md, color = "red") +
-  xlab("Time (16-day intervals)") +
-  ylab("NDVI") +
-  theme_bw() +
-  theme(axis.title.y = element_text(size = rel(1.15), angle = 90),
-        axis.title.x = element_text(size = rel(1.15), angle = 0),
-        strip.text.x = element_text(size = rel(.95), angle = 0),
-        ggsave(filename = "cldcv_boxp_ts2.png", units = "mm",
-               height = 300, dpi = 600))
-
-
-ggplot() + 
-  geom_boxplot(data = ndvi.lu_bp, aes(x = date, y = value, group = date, 
-                                      fill = season), range = 0, 
-               colour = "transparent", outlier.shape = "*") + 
-  scale_fill_manual(name = "Season", values = colors) + 
-  geom_line(aes(x = date, y = kza(value, m = 13 * 30 / 16, k = 3, impute_tails = T)$kz, 
-                group = plot, alpha = 50), 
-            data = ndvi.lu_bp, color = "grey70") +
-  facet_wrap(~ type, nrow = 6, ncol = 2) + 
-  #   scale_x_continuous(breaks = seq(2002, 2014, 1), 
-  #                      labels = c("2002", "", "2004", "", "2006", "", "2008", "", 
-  #                                 "2010", "", "2012", "", "")) + 
-  geom_line(aes(x = date, y = kz), data = ndvi.lu_md, color = "red") + 
-  xlab("Time [d]") + 
-  ylab("Optical thickness") + 
-  theme_bw()
-ggsave(filename = "cldcv_boxp_all_qa_outlier2.png", units = "mm", 
-       width = 240 * 2, height = 120 * 6)
+        strip.text.x = element_text(size = rel(.95), angle = 0)) 
+ggsave(filename = "out/plots/cc_ndvi_boxp_ts.png", units = "mm", 
+       height = 300, dpi = 600)
 dev.off()
-
-# ## Boxplot seasonal time series
-# 
-# ndvi.lu_bp$month <- strftime(ndvi.lu_bp[, 1], format = "%b")
-# 
-# ndvi.lu_bp$season <- NA
-# 
-# # JF, MAM, JJAS, OND
-# ndvi.lu_bp$season[ndvi.lu_bp$month %in% c("Jan", "Feb")] <- "JF"
-# ndvi.lu_bp$season[ndvi.lu_bp$month %in% c("Mar", "Apr", "May")] <- "MAM"
-# ndvi.lu_bp$season[ndvi.lu_bp$month %in% c("Jun", "Jul", "Aug", "Sep")] <- "JJAS"
-# ndvi.lu_bp$season[ndvi.lu_bp$month %in% c("Oct", "Nov", "Dec")] <- "OND"
-# 
-# ndvi.lu_bp$season <- paste(ndvi.lu_bp$plot, ndvi.lu_bp$season, 
-#                            ifelse(ndvi.lu_bp$month %in% c("Jan", "Feb"), 
-#                                   as.numeric(substr(ndvi.lu_bp$date, 1, 4)) - 1, 
-#                                   substr(ndvi.lu_bp$date, 1, 4)), sep = "_")
-# 
-# tmp.df.bp <- subset(ndvi.lu_bp, substr(plot, 1, 3) == "sav")
-# 
-# tmp.agg <- aggregate(tmp.df.bp$value, by = list(tmp.df.bp$season), FUN = max)
-# tmp.agg$date <- sapply(strsplit(tmp.agg[, 1], "_"), "[[", 3)
-# 
-# tmp.agg$season <- sapply(strsplit(tmp.agg[, 1], "_"), "[[", 2)
-# tmp.agg$season[tmp.agg$season == "JF"] <- "01"
-# tmp.agg$season[tmp.agg$season == "MAM"] <- "04"
-# tmp.agg$season[tmp.agg$season == "JJAS"] <- "07"
-# tmp.agg$season[tmp.agg$season == "OND"] <- "11"
-# 
-# tmp.agg$date <- as.Date(paste(tmp.agg$date, tmp.agg$season, "01", sep = "-"))
-# tmp.agg <- tmp.agg[order(tmp.agg$date), ]
-# 
-# # Plotting
-# ggplot() + 
-#   geom_boxplot(data = tmp.agg, 
-#                aes(x = date, y = x, group = date)) + 
-#   geom_line(aes(x = tmp.df$date, y = tmp.df$kz), color = "red")
 
 
 ## Cloud cover change detection 2003-2012
 
-ndvi.cc.fls <- list.files("data/cloud_cover", full.names = T)
-ndvi.cc.rst <- stack(ndvi.cc.fls[substr(basename(ndvi.cc.fls), 1, 4) %in% 
+ndvi.cc.fls <- list.files("data/cloud_cover", full.names = T, pattern = "absolute")
+ndvi.cc.rst <- stack(ndvi.cc.fls[substr(basename(ndvi.cc.fls), 10, 13) %in% 
                                    as.character(2003:2012)])
 
 ndvi.cc.mat <- as.matrix(ndvi.cc.rst)
@@ -384,9 +307,72 @@ ndvi.cc <- foreach(i = luc, .packages = lib, .combine = "rbind") %dopar% {
   } 
 }
 
-ggplot() + 
-  geom_histogram(data = ndvi.cc, stat = "identity", aes(x = date, y = value))
+# Chi-squared and labels
+chi.squ <- function(x) {
+  tmp.chi <- round(chisq.test(x)$p.value, 2)
+  return(paste("p", as.character(tmp.chi), sep = " = "))
+}
 
+lab <- foreach(i = unique(ndvi.cc$plot), .combine = "c") %do% {
+  tmp <- subset(ndvi.cc, plot == i)$value
+  return(chi.squ(tmp))
+}
+
+ndvi.cc$p.value <- NA
+for (i in seq(unique(ndvi.cc$plot))) {
+  ndvi.cc$p.value[grep(unique(ndvi.cc$plot)[i], ndvi.cc$plot)] <- lab[i]
+}
+
+
+## GGplot style (without NDVI time series)
+ggplot(aes(x = as.factor(date), y = value), data = ndvi.cc) + 
+  geom_histogram(stat = "identity") +
+  facet_wrap(~ plot, nrow = 12, ncol = 5) + 
+  geom_text(aes(x = "2011", y = 22, label = p.value), color = "grey40") + 
+  theme_bw() + 
+  xlab("Time [y]") + 
+  ylab("Total amount of cloudy pixels")
+ggsave(filename = "out/plots/ndvi_boxp_ts.png", units = "mm", 
+       width = 240 * 2, height = 120 * 4)
+dev.off()
+
+
+## Lattice style (including NDVI time series)
+
+# Cloud cover plot
+plot.cc <- xyplot(value ~ date | plot, data = ndvi.cc, panel = function(x, y) {
+  panel.barchart(x, y, horizontal = F, box.ratio = 2, col = "grey70")
+  
+}, xlab = "", ylab = "Total amount of cloudy pixels", layout = c(5, 12))
+
+# NDVI plot
+ndvi.lu_bp$date_num <- as.POSIXlt(ndvi.lu_bp$date, format = "%Y-%m%-d")
+ndvi.lu_bp$date_num <- 1900 + ndvi.lu_bp$date_num$year + ndvi.lu_bp$date_num$yday / 366
+
+plot.ndvi <- xyplot(value ~ date_num | plot, data = ndvi.lu_bp, panel = function(x, y) {
+  panel.xyplot(x, y, type = "l", col = "grey30")
+  panel.lines(x, kza(y, m = 13 * 30 / 16, impute_tails = T)$kz, 
+              col = brewer.pal(5, "YlOrBr")[5])
+}, xlab = "", ylab = "NDVI", layout = c(5, 12))
+
+# Combine cloud cover and NDVI plots
+t <- trellis.par.get()
+t$layout.heights$strip <- 0.7
+t$strip.background$col <- brewer.pal(5, "BuGn")[2]
+
+png(filename = "out/plots/cc_ndvi_lattice.png", width = 500, units = "mm", 
+    res = 300, pointsize = 16)
+update(doubleYScale(plot.ndvi, plot.cc, add.ylab2 = T, 
+             style1 = 0, style2 = 0, under = T), asp = 0.25, 
+      par.settings = t)
+# Add p-value information 
+for (i in seq(12)) {
+  for (j in seq(5)) {
+    grid::seekViewport(trellis.vpname("panel", j, i))
+    grid::grid.text(label = lab[i * j], x = .92, y = .1, gp = gpar(fontsize = 10))
+  }
+}
+dev.off()
 
 # Deregister parallel  backend
 stopCluster(cl)
