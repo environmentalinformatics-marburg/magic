99504d11773a09db697fabf66a087b57a8d8ff03
IOtte <insa.otte@gmx.de>
2015-01-13 17:38:57 +0100

    some changes

--- dfg_for_kilimanjaro/stable_isotope_analysis/stable_isotopes.R
+++ dfg_for_kilimanjaro/stable_isotope_analysis/stable_isotopes.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(lubridate)
+# library(plyr) 
 
 
 # Check your working directory
@@ -14,6 +15,13 @@ iso <- read.csv2("iso_calc.csv", header = T)
 col.id <- c("orange", "red", "black", "yellow", "blue", "purple", "green", 
             "pink", "cornflowerblue", "white", "brown")
 col.id.1 <- c("orange", "black", "blue", "purple", "green", "brown")
+col.id.2 <- c("orange", "blue", "purple", "black", "yellow", "red", "white", 
+              "green", "brown")
+col.id.3 <- c("orange", "red", "black", "yellow", "purple")
+
+# sort legend
+legend.order <- c("fer0", "fpd0", "fpo0", "foc0", "foc6", "flm1", "nkw1", 
+                  "hom4", "sav5")
 
 
 ### Find regression line for all types of precipitation 
@@ -38,6 +46,7 @@ sc.pl.lmwl.gmwl <- sc.pl.lmwl + geom_abline(intercept = 10, slope = 8)
 
 
 # regressions for rain, fog & throughfall for all data pairs available
+## -> lapply()
 sm.rain <- summary(lm(dD_H ~ d18_16, data = subset(iso, iso$type == "rain")))
 sm.fog <- summary(lm(dD_H ~ d18_16, data = subset(iso, iso$type == "fog"))) 
 sm.tf <- summary(lm(dD_H ~ d18_16, data = subset(iso, iso$type == "tf")))
@@ -56,12 +65,7 @@ iso.machame <- subset(iso, iso$plot_id_sp1 != "hom4" &
                         iso$plot_id_sp1 != "mnp1" &
                         iso$plot_id_sp1 != "mnp2")
 
-# plot for overview 
 
-col.id.machame <- c("orange", "red", "black", "yellow", "purple")
-sc.pl.machame <- qplot(d18_16, dD_H, data = iso.machame, color = plot_id_sp1, 
-                       shape = type, xlab = "d18O%oGMWL", ylab = "d2H%oGMWL") + 
-  scale_color_manual(values = col.id.machame)
 
 
 
@@ -82,6 +86,29 @@ sm.fog.machame <- summary(lm(dD_H ~ d18_16, data = subset(iso.machame, iso.macha
 # machame throughfall 
 #machame.tf <- subset(iso.machame, iso.machame$type == "tf")
 sm.tf.machame <- summary(lm(dD_H ~ d18_16, data = subset(iso.machame, iso.machame$type == "tf"))) 
+
+
+# plot for overview 
+
+col.id.machame <- c("orange", "purple", "black", "yellow", "red")
+
+# Achsenbeschriftung formatieren --> Sonderzeichen???
+# --> ??
+sc.pl.machame <- qplot(d18_16, dD_H, data = iso.machame, color = plot_id_sp1, 
+                       shape = type, xlab = "d18O%o", ylab = "d2H%o") + 
+  scale_color_manual(values = col.id.machame, limits = c("fer0", "fpo0", "foc0", "foc6", "flm1")) +
+  geom_abline(intercept = 10, slope = 8, linetype = 2) + 
+  geom_abline(intercept = 16.88, slope = 7.78)
+
+
+
+
+sc.pl.machame.all <- qplot(d18_16, dD_H, data = iso.machame, color = plot_id_sp1, 
+                       shape = type, xlab = "d18O%o", ylab = "d2H%o") + 
+  facet_wrap( ~ type) +
+  scale_color_manual(values = col.id.machame, limits = c("fer0", "fpo0", "foc0", "foc6", "flm1")) +
+  geom_abline(intercept = 10, slope = 8, linetype = 2) + 
+  geom_abline(intercept = 16.88, slope = 7.78)
 
 
 
@@ -145,12 +172,13 @@ iso$yrweek <- paste(iso$year, iso$week, sep = "-")
 
 
 # build monthly mean values of d18-16 & dD_H
-iso.mns <- aggregate(cbind(iso$d18_16, iso$dD_H), by = list(iso$yrmn, iso$plot_id_sp1, iso$type),
+iso.mns <- aggregate(cbind(iso$d18_16, iso$dD_H), by = list(iso$yrmn, iso$plot_id_sp1, iso$type, 
+                                                            iso$elevation),
                      FUN = "mean", na.rm = TRUE)
 colnames(iso.mns) <- c("date", "plot_id_sp1", "type", "d18_16", "dD_H")
 
 # build monthly sums of amount_mm
-amnt.mns <- aggregate(iso$amount_mm, by = list(iso$yrmn, iso$plot_id_sp1, iso$type), 
+amnt.mns <- aggregate(iso$amount_mm, by = list(iso$yrmn, iso$plot_id_sp1, iso$type, iso$elevation), 
                       FUN = "sum", na.rm = TRUE)
 colnames(amnt.mns) <- c("date", "plot_id_sp1", "type", "amount_mm")
 
@@ -163,11 +191,82 @@ iso.mns.all <- merge(amnt.mns, iso.mns)
 # plotting stuff
 
 iso.mns.all$date <- factor(iso.mns.all$date)
-### --> ???
-iso.mns.all.pl <- ggplot(iso.mns.all, aes(x = date, y = d18_16, group = 1),
-                         shape = type) + geom_line() 
+
+### d18_16O
+iso.mns.all.18O.pl <- ggplot(subset(iso.mns.all, 
+                                  iso.mns.all$plot_id_sp1 != "mnp1" &
+                                  iso.mns.all$plot_id_sp1 != "mnp2"), 
+                         aes(x = date, y = d18_16, group = plot_id_sp1, 
+                             colour = plot_id_sp1)) + 
+  facet_grid(type ~ ., scales = "free") +
+  geom_line() + 
+  scale_color_manual(values = col.id.2, limits = legend.order, name = "Plot ID SP1") + 
+  labs(x = "", y = "d18O%o") +
+  theme(
+    panel.grid.major = element_line(color = "lightgray", size = 0.01),
+    panel.background = element_rect(fill = NA),
+    panel.border = element_rect(color = "gray", fill = NA))
+
+
+# print "iso.mns.all.18O.pl"
+png("out/iso.mns.all.18O.pl.2.png", width = 30, height = 20, units = "cm", 
+    res = 300, pointsize = 15)
+print(iso.mns.all.18O.pl)
+dev.off()
+
+
+# strange values since 2014-05 --> visual check
+iso.mns.all.18O.1405.pl <- ggplot(subset(iso.mns.all, 
+                                    iso.mns.all$plot_id_sp1 != "mnp1" &
+                                      iso.mns.all$plot_id_sp1 != "mnp2" & 
+                                      iso.mns.all$plot_id_sp1 != "hom4" &
+                                      iso.mns.all$plot_id_sp1 != "sav5" &
+                                      iso.mns.all$plot_id_sp1 != "fpd0" &
+                                      iso.mns.all$plot_id_sp1 != "nkw1" &
+                                      iso.mns.all$date > "2014-05"),
+                             aes(x = date, y = d18_16, group = plot_id_sp1, 
+                                 colour = plot_id_sp1)) + 
+  facet_grid(type ~ .) +
+  geom_line() + 
+  scale_color_manual(values = col.id.3) + 
+  labs(x = "", y = "d18O%o")
 
 
 
 
+
+### dD_H
+iso.mns.all.dDH.pl <- ggplot(subset(iso.mns.all, 
+                                    iso.mns.all$plot_id_sp1 != "mnp1" &
+                                    iso.mns.all$plot_id_sp1 != "mnp2"), 
+                             aes(x = date, y = dD_H, group = plot_id_sp1, 
+                                 colour = plot_id_sp1)) + 
+  facet_grid(type ~ .) +
+  geom_line() + 
+  scale_color_manual(values = col.id.2) + 
+  labs(x = "", y = "d2H%o")
+
+
+# print "iso.mns.all.dDH.pl"
+png("out/iso.mns.all.dDH.pl.png", width = 30, height = 20, units = "cm", 
+    res = 300, pointsize = 15)
+print(iso.mns.all.dDH.pl)
+dev.off()
+
+
+# strange values since 2014-05 --> visual check
+iso.mns.all.d2H.1405.pl <- ggplot(subset(iso.mns.all, 
+                                         iso.mns.all$plot_id_sp1 != "mnp1" &
+                                           iso.mns.all$plot_id_sp1 != "mnp2" & 
+                                           iso.mns.all$plot_id_sp1 != "hom4" &
+                                           iso.mns.all$plot_id_sp1 != "sav5" &
+                                           iso.mns.all$plot_id_sp1 != "fpd0" &
+                                           iso.mns.all$plot_id_sp1 != "nkw1" &
+                                           iso.mns.all$date > "2014-05"),
+                                  aes(x = date, y = dD_H, group = plot_id_sp1, 
+                                      colour = plot_id_sp1)) + 
+  facet_grid(type ~ .) +
+  geom_line() + 
+  scale_color_manual(values = col.id.3) + 
+  labs(x = "", y = "d2H%o")
 
