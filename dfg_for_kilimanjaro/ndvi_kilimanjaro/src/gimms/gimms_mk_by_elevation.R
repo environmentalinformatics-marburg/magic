5c5305842b943ede3b7fa6557d1f5744f91dc51c
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-01-27 16:41:16 +0000

    Update: included Mann-Kendall significance < .05 (similar to Pettorelli et al. - 2012) in NP greening analysis.

--- dfg_for_kilimanjaro/ndvi_kilimanjaro/src/gimms/gimms_mk_by_elevation.R
+++ dfg_for_kilimanjaro/ndvi_kilimanjaro/src/gimms/gimms_mk_by_elevation.R
@@ -1,3 +1,5 @@
+source("kendallStats.R")
+
 # dem
 dem <- raster("data/DEM_ARC1960_30m_Hemp.tif")
 
@@ -9,9 +11,12 @@ mod_predicted_mk <- lapply(mod_predicted_mk, raster)
 
 dem_rsmpl <- resample(dem, mod_predicted_mk[[4]])
 
-tmp <- mod_predicted_mk[[4]]
-tmp[which(dem_rsmpl[] < 3500)] <- NA
+tmp <- mod_predicted_mk[[1]]
+
+lapply(seq(2000, 5000, 1000), function(i) {
+tmp[which(dem_rsmpl[] < i)] <- NA
 
 kendallStats(tmp)
+})
 
 overlay(mod_predicted_mk[[4]], dem_rsmpl)
