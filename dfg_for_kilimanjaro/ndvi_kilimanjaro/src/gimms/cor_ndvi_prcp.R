e68418652cd1d7bd21a6fdee564bc1b6fe189ee2
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-02-26 11:00:03 +0100

    Update: implemented aggregation of monthly rainfall per ENSO/IOD period to annual amounts.

--- dfg_for_kilimanjaro/ndvi_kilimanjaro/src/gimms/cor_ndvi_prcp.R
+++ dfg_for_kilimanjaro/ndvi_kilimanjaro/src/gimms/cor_ndvi_prcp.R
@@ -40,3 +40,58 @@ dev.off()
 calc(rst_ndvi, fun = function(x) {
   cor(x, dat_prcp_kia_mos[, 2])
 })
+
+
+## aggregated rainfall amounts per season
+# el nino
+groups_nino <- list("all El Ninos", "pure El Ninos", "pure m/s El Ninos", 
+               "El Ninos w IOD+", "m/s El Ninos w IOD+", "purest IOD+")
+
+types_nino <- list(c("WE", "ME", "SE"),
+                    c("WE", "ME", "SE"),
+                    c("ME", "SE"), 
+                    c("WE", "ME", "SE"), 
+                    c("ME", "SE"), 
+                    "WE")
+
+groups_iod <- list(c("M", "", "P"), 
+                   "", 
+                   "", 
+                   "P", 
+                   "P", 
+                   "P")
+
+nino <- types_nino[[1]]
+iod <- groups_iod[[1]]
+group <- groups_nino[[1]]
+span <- 0
+
+prcp_agg_group <- foreach(nino = types_nino, iod = groups_iod, 
+                          group = groups_nino, .combine = "rbind") %do% {
+  
+  # subset oni/dmi                   
+  oni_mlt_sub <- subset(oni_mlt, (Type %in% nino) & (IOD %in% iod))
+
+  # remove incomplete data (e.g. late 2011)
+  seasons <- split(oni_mlt_sub, as.factor(oni_mlt_sub$Season))
+  seasons_len <- sapply(seasons, nrow)
+  seasons <- seasons[seasons_len == 12]
+  
+  oni_mlt_sub <- do.call("rbind", seasons)
+  
+  # merge with corresponding rainfall records
+  df_oni_prcp <- merge(oni_mlt_sub, dat_prcp_kia_mos, 
+                       by.x = "Date", by.y = "YEAR", all.x = TRUE)
+  
+  # aggregate (sum) rainfall amounts per season
+  df_oni_prcp_aggssn <- aggregate(df_oni_prcp[, c("P_KIA_NEW", "P_MOSHI_NEW", "P_MEAN_NEW")], 
+                                  by = list(df_oni_prcp$Season), FUN = sum)
+  
+  # aggregate (mean) rainfall amounts per current nino group
+  num_oni_prcp_aggall <- colMeans(df_oni_prcp_aggssn[, c("P_KIA_NEW", "P_MOSHI_NEW", "P_MEAN_NEW")])
+  mat_oni_prcp_aggall <- matrix(num_oni_prcp_aggall, byrow = TRUE, ncol = 3)
+  df_oni_prcp_aggall <- data.frame(Group = group, mat_oni_prcp_aggall)
+  names(df_oni_prcp_aggall)[2:4] <- names(num_oni_prcp_aggall)
+  
+  return(df_oni_prcp_aggall)
+}
