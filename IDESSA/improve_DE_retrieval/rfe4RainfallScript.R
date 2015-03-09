833f9caec7492b67627721ac4e76702c3dd99ce1
Hanna <hanna.meyer1@gmail.com>
2015-03-09 13:54:10 +0100

    initial commit

---
+++ IDESSA/improve_DE_retrieval/rfe4RainfallScript.R
@@ -0,0 +1,29 @@
+
+library(Rainfall)
+library(doParallel)
+################################################################################
+### User adjustments ###########################################################
+resultpath="/media/memory18201/casestudies/hmeyer/Improve_DE_retrieval/results"
+datasetTime<-"day"
+responseName<-"RInfo"
+sampsize=0.05#0.05 for rinfo, 1 for rain
+
+
+################################################################################
+load(paste0(resultpath,"/datatable_",datasetTime,".RData"))
+
+predictors<-datatable[,4:(ncol(datatable)-1)]
+response<-datatable$Radar
+if (responseName=="RInfo"){
+response[datatable$Radar<=0.06]="NoRain"
+response[datatable$Radar>0.06]="Rain"
+library(caret)
+
+rfeModel<-rfe4rainfall(predictors=predictors,response=response,
+                       sampsize=sampsize,
+                       nnetSize=c(seq(2,10,2),seq(20,ncol(predictors),20)),
+                       varSize=c(1:5,seq(10,ncol(predictors),5)))
+
+
+
+save(rfeModel,file=paste0(resultpath,"/rfeModel_",datasetTime,"_",responseName,".Rdata"))
