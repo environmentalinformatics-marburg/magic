92c81335ad06f3b1802206864b13f9d6f7f11a65
dogbert <sforteva@yahoo.de>
2015-06-18 11:37:40 +0200

    backup

--- backup/src/182_local/mount.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This script mounted the 192.168.191.182//media/memory01 
-
-sshfs dogbert@192.168.191.182:/media/memory01/ei_data_kilimanjaro /mnt/sd19006/
-
-# Prozess im Hintergrund starten mit bg -%(Prozessnummer)
-
