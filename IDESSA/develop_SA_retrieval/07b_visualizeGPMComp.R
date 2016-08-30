95e177c9486655b8c3787def9cbcae5a42873545
Hanna <hanna.meyer1@gmail.com>
2016-08-30 18:18:37 +0200

    initial commits

---
+++ IDESSA/develop_SA_retrieval/07b_visualizeGPMComp.R
@@ -0,0 +1,105 @@
+library(Rsenal)
+library(reshape2)
+library(vioplot)
+
+outpath <- "/home/hanna/Documents/Presentations/Paper/in_prep/Meyer2016_SARetrieval/figureDrafts/"
+dat<-get(load("Evaluation/IMERGComparison.RData"))
+results_rate <- list()
+results_rate[[1]]<-data.frame()
+results_rate[[2]]<-data.frame()
+results_area <- list()
+results_area[[1]]<-data.frame()
+results_area[[2]]<-data.frame()
+dat$Day <- substr(dat$Date.x,1,8)
+dat_area <- dat
+
+
+################################################################################
+#RATE
+################################################################################
+dat <- dat[dat$RA_obs=="Rain",]
+dat_agg_day <- aggregate(dat[,names(dat)%in%c("RR_obs","RR_pred","IMERG")],
+                          by=list(dat$Day,dat$Station),sum,na.rm=TRUE)
+
+
+for (i in unique(dat_agg_day$Day)){
+  subs <- dat_agg_day[dat_agg_day$Date.x==i,]
+  if (nrow(subs)<5){next}
+  if(sum(subs$RA_obs=="Rain")<5){next}
+  results_rate[[1]] <- data.frame(rbind(results_rate[[1]],
+                                   data.frame("Date"=i,
+                                              regressionStats(subs$RR_pred,
+                                                              subs$RR_obs,
+                                                              adj.rsq = FALSE))))
+  
+  results_rate[[2]] <- data.frame(rbind(results_rate[[2]],
+                                        data.frame("Date"=i,
+                                                   regressionStats(subs$IMERG,
+                                                                   subs$RR_obs,
+                                                                   adj.rsq = FALSE))))
+}
+results_rate[[2]]$Rsq[is.na(results_rate[[2]]$Rsq)]<-0
+results_rate[[2]]$Model<-"IMERG"
+results_rate[[1]]$Model<-"MSG"
+results<-rbind(results_rate[[1]],results_rate[[2]])
+
+
+results_melt <- melt(results)
+results_melt <- results_melt[results_melt$variable%in%(c("Rsq","RMSE","ME","MAE")),]
+results_melt$Model<-factor(results_melt$Model,levels=c("MSG","IMERG"))
+results_melt$variable<-factor(results_melt$variable,levels=c("Rsq","RMSE","ME","MAE"))
+
+
+pdf(paste0(outpath,"IMERGcomp.pdf"),width=7,height=7)
+bwplot(results_melt$value~results_melt$Model|results_melt$variable,
+       notch=TRUE,ylab="value",
+      scales = "free",fill="lightgrey",
+      par.settings=list(plot.symbol=list(col="black",pch=8,cex=0.4),
+      strip.background=list(col="lightgrey"),
+      box.umbrella = list(col = "black"),
+      box.dot = list(col = "black", pch = 16, cex=0.4),
+      box.rectangle = list(col="black",fill= rep(c("black", "black"),2))))
+dev.off()
+
+pdf(paste0(outpath,"vioplot.pdf"),width=6,height=6)
+vioplot(dat_agg_day$RR_obs,dat_agg_day$RR_pred,dat_agg_day$IMERG,
+        names=c("Rain gauge","MSG","IMERG"),col="grey")
+title(ylab="daily rainfall (mm)")
+dev.off()
+################################################################################
+#Area
+################################################################################
+for (i in unique(dat_area$Date.x)){
+  subs <- dat_area[dat_area$Date.x==i,]
+  results_area[[1]] <- data.frame(rbind(results_area[[1]],
+                                        data.frame("Date"=i,
+                                                   classificationStats(subs$RA_pred,
+                                                                   subs$RA_obs))))
+  
+  results_area[[2]] <- data.frame(rbind(results_area[[2]],
+                                        data.frame("Date"=i,
+                                                   classificationStats(subs$RA_IMERG,
+                                                                   subs$RA_obs))))
+}
+results_area[[2]]$Model<-"IMERG"
+results_area[[1]]$Model<-"MSG"
+results<-rbind(results_area[[1]],results_area[[2]])
+
+
+results_melt <- melt(results)
+results_melt <- results_melt[results_melt$variable%in%(c("POD","PFD","FAR","HSS")),]
+results_melt$Model<-factor(results_melt$Model,levels=c("MSG","IMERG"))
+results_melt$variable<-factor(results_melt$variable,levels=c("POD","PFD","FAR","HSS"))
+
+
+pdf(paste0(outpath,"IMERGcomp_area.pdf"),width=7,height=7)
+bwplot(results_melt$value~results_melt$Model|results_melt$variable,
+       notch=TRUE,ylab="value",
+       scales = "free",fill="lightgrey",
+       par.settings=list(plot.symbol=list(col="black",pch=8,cex=0.4),
+                         strip.background=list(col="lightgrey"),
+                         box.umbrella = list(col = "black"),
+                         box.dot = list(col = "black", pch = 16, cex=0.4),
+                         box.rectangle = list(col="black",fill= rep(c("black", "black"),2))))
+dev.off()
+
