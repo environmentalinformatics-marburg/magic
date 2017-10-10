6aacb86b73032c08e849a6517aa19489cdee0fcb
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-10-10 15:21:19 +0200

    Minor edits.

--- dfg_for_bale/p3/R/map-plots.R
+++ dfg_for_bale/p3/R/map-plots.R
@@ -6,10 +6,8 @@ setwd("dfg_for_bale/p3")
 ## libraries and functions
 # devtools::install_github("environmentalinformatics-marburg/Rsenal", local = FALSE)
 # devtools::install_github("fdetsch/Orcs", local = FALSE)
-library(Orcs)
-library(foreach)
-library(grid)
-library(latticeExtra)
+lib = c("Orcs", "Rsenal", "latticeExtra", "grid")
+Orcs::loadPkgs(lib)
 
 source("../p1/R/panel.smoothconts.R")
 source("../p1/R/visDEM.R")
@@ -51,12 +49,16 @@ dem = if (file.exists(nms)) {
 ### visualize study area -----
 
 ## retrieve aerial image
-rgb = OpenStreetMap::openmap(c(ymax(ext), xmin(ext)), c(ymin(ext), xmax(ext))
-                             , type = "bing", minNumTiles = 40L)
-rgb = raster(rgb)
-rgb = trim(projectRaster(rgb, crs = "+init=epsg:32637", method = "ngb")
-           , filename = "data/osm_bale.tif", datatype = "INT1U"
-           , overwrite = TRUE)
+nms = "data/osm_bale.tif"
+rgb = if (file.exists(nms)) {
+  brick(nms)
+} else {
+  tmp = OpenStreetMap::openmap(c(ymax(ext), xmin(ext)), c(ymin(ext), xmax(ext))
+                               , type = "bing", minNumTiles = 40L)
+  tmp = raster(tmp)
+  trim(projectRaster(tmp, crs = "+init=epsg:32637", method = "ngb")
+       , filename = nms, datatype = "INT1U")
+}
 
 ## create contour lines
 p_dem = visDEM(dem, seq(1250, 4500, 50), col = "black", labcex = .6
@@ -64,22 +66,23 @@ p_dem = visDEM(dem, seq(1250, 4500, 50), col = "black", labcex = .6
 
 ## create figures, one with contour lines superimposed upon a semi-transparent
 ## satellite image, and one with the non-transparent satellite image only
-foreach(alpha = c(.8, 1), nms = c("map_bale_dem.tiff", "map_bale.tiff")) %do% {
-  rsl = Rsenal::rgb2spLayout(rgb, quantiles = c(0, 1), alpha = alpha)
-          
+alpha = c(.8, 1); nms = c("map_bale_dem.tiff", "map_bale.tiff")
+for (i in 1:2) {
+  rsl = Rsenal::rgb2spLayout(rgb, quantiles = c(0, 1), alpha = alpha[i])
+  
   p_bale = spplot(rgb[[1]], col.regions = "transparent", colorkey = FALSE
                   , scales = list(draw = TRUE, cex = .8, alternating = 3
                                   , x = list(at = seq(560000, 620000, 10000))
                                   , y = list(rot = 90))
                   , sp.layout = rsl
                   , maxpixels = ncell(rgb))
-
-  if (alpha != 1) {
+  
+  if (alpha[i] != 1) {
     p_bale = p_bale + as.layer(p_dem)
   }
   
-  tiff(file.path("img", nms), width = 42, height = 29.7, units = "cm", res = 500
-       , compression = "lzw")
+  tiff(file.path("img", nms[i]), width = 42, height = 29.7, units = "cm"
+       , res = 300, compression = "lzw")
   plot.new()
   
   print(p_bale + 
@@ -109,10 +112,10 @@ foreach(alpha = c(.8, 1), nms = c("map_bale_dem.tiff", "map_bale.tiff")) %do% {
 }
 
 
-### visualize settlements -----
+### visualize settlements (OpenStreetMap) -----
 
 for (i in 1:length(settlements)) {
-
+  
   stm = tolower(settlements[i, ]@data$name)
   lbl = paste(sapply(strsplit(stm, " "), Hmisc::capitalize), collapse = " ")
   stm = gsub(" ", "-", stm)
@@ -122,8 +125,6 @@ for (i in 1:length(settlements)) {
   ## retrieve aerial image
   bff = rgeos::gBuffer(settlements[i, ], width = 2000, quadsegs = 250)
   ext = extend(extent(bff), c(1e3, 100))
-  # rgb = dismo::gmap(ext2spy(ext, "+init=epsg:32637"), type = "satellite"
-  #                   , scale = 2, rgb = TRUE)
   ext = spTransform(ext2spy(ext, "+init=epsg:32637"), CRS("+init=epsg:4326"))
   
   nms = paste0(file.path("data/osm", paste0("osm_", stm)), ".tif")
@@ -136,6 +137,75 @@ for (i in 1:length(settlements)) {
     
     trim(projectRaster(tmp, crs = "+init=epsg:32637", method = "ngb")
          , filename = nms, datatype = "INT1U")
+  }
+  
+  ## create contour lines
+  tmp = crop(dem, rgb, snap = "out")
+  p_dem = visDEM(tmp, seq(1250, 4500, 10), col = "black", labcex = .6
+                 , labels = NULL)
+  
+  ## create figures, one with contour lines superimposed upon a semi-transparent
+  ## satellite image, and one with the non-transparent satellite image only
+  nms = c(paste("map", stm, "dem.tiff", sep = "_"), paste0("map_", stm, ".tiff"))
+  for (j in 1:2) {
+    rsl = rgb2spLayout(rgb, quantiles = c(0, 1), alpha = alpha[j])
+    
+    p_bale = spplot(rgb[[1]], col.regions = "transparent", colorkey = FALSE
+                    , scales = list(draw = TRUE, cex = .8, alternating = 3
+                                    , y = list(rot = 90))
+                    , sp.layout = rsl, maxpixels = ncell(rgb))
+    
+    if (alpha[j] != 1) {
+      p_bale = p_bale + as.layer(p_dem)
+    }
+    
+    tiff(file.path("img/settlements/osm", nms[j]), width = 42, height = 29.7
+         , units = "cm", res = 300, compression = "lzw")
+    plot.new()
+    
+    crd = as.numeric(coordinates(settlements[i, ]))
+    crd[1] = crd[1] + 250
+    print(p_bale + 
+            layer(sp.points(settlements[i, ], pch = 24, col = "white"
+                            , fill = "black", cex = 2, lwd = 2)) + 
+            layer(sp.polygons(bff, lty = 2, lwd = 2)), newpage = FALSE)
+    
+    # epsg code
+    downViewport(trellis.vpname("figure"))
+    grid.stext("EPSG:32637\n\uA9 OpenStreetMap contributors"
+               , x = unit(.99, "npc"), y = unit(.01, "npc")
+               , just = c("right", "bottom") , gp = gpar(cex = .8))
+    grid.stext(lbl, gp = gpar(cex = 1.8), just = c("left", "bottom")
+               , x = unit(.52, "npc"), y = unit(.5, "npc"))
+    
+    invisible(dev.off())
+  }
+}
+
+
+### visualize settlements (dismo) -----
+
+for (i in 1:length(settlements)) {
+  
+  stm = tolower(settlements[i, ]@data$name)
+  lbl = paste(sapply(strsplit(stm, " "), Hmisc::capitalize), collapse = " ")
+  stm = gsub(" ", "-", stm)
+  
+  cat(lbl, "is in, start processing.\n")
+  
+  ## retrieve aerial image
+  bff = rgeos::gBuffer(settlements[i, ], width = 2000, quadsegs = 250)
+  ext = extend(extent(bff), c(1e3, 100))
+  
+  ext_dsm = extend(ext, c(0, 0, 250, 0))
+  nms_dsm = paste0(file.path("data/dsm", paste0("dsm_", stm)), ".tif")
+  rgb = if (file.exists(nms_dsm)) {
+    brick(nms_dsm)
+  } else {
+    tmp = dismo::gmap(ext2spy(ext_dsm, "+init=epsg:32637"), type = "satellite"
+                      , scale = 2, rgb = TRUE)
+    tmp = projectRaster(tmp, crs = "+init=epsg:32637", method = "ngb")
+    crop(tmp, ext, snap = "out", filename = nms_dsm, datatype = "INT1U")
   }
   
   ## create contour lines
@@ -158,7 +228,7 @@ for (i in 1:length(settlements)) {
       p_bale = p_bale + as.layer(p_dem)
     }
     
-    tiff(file.path("img/settlements", nms), width = 42, height = 29.7
+    tiff(file.path("img/settlements/dsm", nms), width = 42, height = 29.7
          , units = "cm", res = 300, compression = "lzw")
     plot.new()
     
@@ -171,7 +241,7 @@ for (i in 1:length(settlements)) {
     
     # epsg code
     downViewport(trellis.vpname("figure"))
-    Rsenal::grid.stext("EPSG:32637\n\uA9 OpenStreetMap contributors"
+    Rsenal::grid.stext("EPSG:32637\n\uA9 Google, TerraMetrics"
                        , x = unit(.99, "npc"), y = unit(.01, "npc")
                        , just = c("right", "bottom") , gp = gpar(cex = .8))
     Rsenal::grid.stext(lbl, gp = gpar(cex = 1.8), just = c("left", "bottom")
