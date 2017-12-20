3d7fc80c47227c1fb7f9d85b2d5ac121977bc761
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-12-20 14:39:07 +0000

    Initial upload: MODIS parallel download.

---
+++ Tair4Antarctica/R/modis-process.R
@@ -0,0 +1,36 @@
+### environment -----
+
+library(MODIS)
+library(doParallel)
+
+## parallelization
+cl = makeCluster(18L)
+registerDoParallel(cl)
+
+## 'MODIS' package options
+lap = "../../data/MODIS_ARC"
+MODISoptions(lap, file.path(lap, "PROCESSED"), outProj = "+init=epsg:4326"
+             , MODISserverOrder = c("LPDAAC", "LAADS"), quiet = TRUE)
+
+
+### download and extract -----
+
+antarctica = getData(country = "ATA", level = 0, path = "../../data")
+
+## 500-m bands 1 to 7
+mod09ga_hdf = foreach(year = 2000:2017, .packages = "MODIS") %dopar% {
+  getHdf("MOD09GA", extent = antarctica, collection = "006"
+         , begin = paste0(year, ".01.01"), end = paste0(year, ".12.31")
+         , checkIntegrity = FALSE)
+}
+
+
+# swb = runGdal("MOD09GA", extent = antarctica, collection = "006"
+#               , begin = "2000.02.24", end = "2000.02.24"
+#               , SDSstring = "0100000000011111111000", job = "mod09ga-antarctica")
+# 
+# ## 5600-m climate modeling grid (cmg) bands 1 to 7
+# cmg = runGdal("MOD09CMG", extent = antarctica, collection = "006"
+#               , begin = "2000.02.24", end = "2000.02.24"
+#               , SDSstring = "1111111000000000011100000"
+#               , job = "mod09cmg-antarctica")
