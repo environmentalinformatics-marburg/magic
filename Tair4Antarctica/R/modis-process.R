7835d51c0742dba205276c8ecf327274e850c2b5
fdetsch <florian.detsch@staff.uni-marburg.de>
2018-01-31 07:28:50 +0000

    Update: switched to 'parallel' package; parallelized runGdal().

--- Tair4Antarctica/R/modis-process.R
+++ Tair4Antarctica/R/modis-process.R
@@ -1,16 +1,16 @@
 ### environment -----
 
 library(MODIS)
-library(doParallel)
-
-## parallelization
-cl = makeCluster(18L)
-registerDoParallel(cl)
+library(parallel)
 
 ## 'MODIS' package options
 lap = "../../data/MODIS_ARC"
 MODISoptions(lap, outDirPath = file.path(lap, "PROCESSED")
              , MODISserverOrder = c("LPDAAC", "LAADS"), quiet = TRUE)
+
+## parallelization
+cl = makePSOCKcluster(4L)
+jnk = clusterEvalQ(cl, { library(MODIS); MODISoptions() })
 
 
 ### download and extract -----
@@ -19,19 +19,26 @@ MODISoptions(lap, outDirPath = file.path(lap, "PROCESSED")
 ## https://wiki.openstreetmap.org/wiki/Antarctica/Creating_a_map#Projection)
 antarctica = getData(country = "ATA", level = 0, path = "../../data")
 antarctica = spTransform(antarctica, CRS("+init=epsg:3031"))
+
 ata_tls = getTile(antarctica)
+clusterExport(cl, "ata_tls")
 
 ## download (in parallel) and extract 500-m bands 1 to 7 including quality 
 ## information
-mod09ga_hdf = foreach(year = 2000:2018, .packages = "MODIS") %dopar% {
+mod09ga_hdf = parLapply(cl, 2000:2018, function(year) {
   getHdf("MOD09GA", extent = ata_tls, collection = "006"
          , begin = paste0(year, ".01.01"), end = paste0(year, ".12.31")
-         , checkIntegrity = FALSE)
-}
+         , checkIntegrity = FALSE, forceDownload = TRUE)
+})
 
-mod09ga_tfs = runGdal("MOD09GA", extent = ata_tls, collection = "006"
-                      , SDSstring = "0100000000011111111000"
-                      , job = "mod09ga-antarctica")
+mod09ga_tfs = parLapply(cl, 2000:2018, function(year) {
+  runGdal("MOD09GA", extent = ata_tls, collection = "006"
+          , SDSstring = "0100000000011111111000"
+          , job = file.path("mod09ga-antarctica", year)
+          , begin = paste0(year, ifelse(year == 2000, ".02.24", ".01.01"))
+          , end = paste0(year, ".12.31")
+          , checkIntegrity = FALSE)
+})
 
 # ## 5600-m climate modeling grid (cmg) bands 1 to 7
 # cmg = runGdal("MOD09CMG", extent = ata_tls, collection = "006"
