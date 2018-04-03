298dcaa328a954db7bc54802b0a6fab5ed72c602
Hanna <hanna.meyer1@gmail.com>
2018-04-03 13:04:57 +0200

    some updates

--- Tair4Antarctica/ReWork2017/05_mergeData.R
+++ Tair4Antarctica/ReWork2017/05_mergeData.R
@@ -2,7 +2,7 @@
 rm(list=ls())
 library(raster)
 library(lubridate)
-mainpath <- "/media/hanna/data/Antarctica/ReModel2017/"
+mainpath <- "/media/memory02/users/hmeyer/Antarctica/ReModel2017/"
 datapath <- paste0(mainpath,"/data/")
 rdatapath <- paste0(datapath, "/RData/")
 rasterdata <- paste0(datapath,"/raster/")
@@ -10,6 +10,7 @@ Shppath <- paste0(datapath,"/ShapeLayers/")
 modelpath <- paste0(datapath, "/modeldat/")
 
 extracted <- list.files(paste0(rdatapath,"extracted/"),full.names = TRUE,pattern="extract")
+extracted_VIS <- list.files(paste0(rdatapath,"extracted_VIS/"),full.names = TRUE,pattern="extract")
 dailydat <- get(load(paste0(rdatapath,"stationdat_daily.RData")))
 
 ###aggregate LST data
@@ -33,6 +34,20 @@ results$Date <- as.Date(as.numeric(substr(results$Date,5,7)),
                          origin=as.Date(paste0(as.numeric(substr(results$Date,1,4))-1,"-12-31")))
 results_merged <- merge(results,dailydat,by.x=c("Date","Station"),by.y=c("Date","Name"))
 results_merged <- results_merged[complete.cases(results_merged),]
+
+###merge with VIS
+
+results <- list()
+for (i in 1:length(extracted_VIS)){
+  extr <- get(load(extracted_VIS[i]))
+  results[[i]] <- extr
+}
+results <- do.call("rbind",results)
+results$Date <- as.Date(as.numeric(substr(results$Date,5,7)), 
+                        origin=as.Date(paste0(as.numeric(substr(results$Date,1,4))-1,"-12-31"))) 
+results_merged <- merge(results_merged,results,by.x=c("Date","Station"),by.y=c("Date","Station"))
+
+
 
 ###add auxiliary infos
 aux <- get(load(paste0(rdatapath,"aux_extr.RData")))
