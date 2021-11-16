3d88f7c50fb7b23779e1e0011e120c6869de6422
Spaska Forteva <spaska.forteva@geo.uni-marburg.de>
2021-11-16 15:28:13 +0100

    Add files via upload

--- dfg_spp_exploratories/rainfall_extraction/01_extractRadolan.R
+++ dfg_spp_exploratories/rainfall_extraction/01_extractRadolan.R
@@ -1,26 +1,28 @@
-# this script extracts time series of RADOLAN data for each plot in the Exploratories
-#Author: Hanna Meyer
-#Date: 14.01.2018
-
 rm(list=ls())
 library(rgdal)
 library(raster)
-datapath <- "/media/hanna/data/RADOLAN/"
+datapath <- "D:/radolan/"
 tmppath <- paste0(datapath,"tmp/")
-shppath <- "/home/hanna/Documents/Projects/Exploratories/RainfallExtraction/Plots/"
+shppath <- "D:/radolan/Plots/"
 resultpath <- paste0(datapath,"results/")
 rasterOptions(tmpdir=tmppath)
 
-#plots <- readOGR(paste0(shppath,"allExploratories.shp"))
+#plots <- readOGR(paste0(shppath,"all_eps_updated_utm32N.shp"))
 #plots <- spTransform(plots,"+proj=stere +lat_0=90.0 +lon_0=10.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m")
-##plots <- plots[,"name"]
+#plots <- plots[,"name"]
 #plots@coords <- plots@coords[, 1:2]
+#coordinates(plots)
+#plots@data <- cbind(plots@data,coordinates(plots))
+#names(plots@data)[4:5] <- c("lat","lon")
 
 plotPolygons <- readOGR(paste0(shppath,"all_eps_updated_utm32N.shp"))
-plotPolygons <- spTransform(plotPolygons,"+proj=stere +lat_0=90.0 +lon_0=10.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m")
-plots <- SpatialPointsDataFrame(coords = coordinates(plotPolygons), data = data.frame(name=plotPolygons$EP), proj4string = plotPolygons@proj4string)
+plotPolygons <- spTransform(plotPolygons,"+proj=stere +lat_0=90.0  
++lon_0=10.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m")
+plots <- SpatialPointsDataFrame(coords = coordinates(plotPolygons), data = data.frame(name=plotPolygons$EP), proj4string =
+                                  plotPolygons@proj4string)
 
-years <- 2019
+(plots)
+years <- 2021
 
 for (year in years){
   yearpath <- paste0(datapath, year)
@@ -35,13 +37,11 @@ for (year in years){
     projection(file) <- "+proj=stere +lat_0=90.0 +lon_0=10.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m"
     results[[i]] <- extract(file,plots,df=FALSE)
   }
-
+  
   results <- data.frame(do.call("rbind",results))
   names(results) <- plots$name
   results$Date <- strptime(substr(files,nchar(files)-16,nchar(files)-4),
-                          format="%Y%m%d-%H%M")
-  save(results,file=paste0(resultpath,"/RADOLAN_extracted_",year,".RData"))
+                           format="%Y%m%d-%H%M")
+  save(results,file=paste0(resultpath,"RADOLAN_extracted_",year,".RData"))
   print(paste0("year ",year, " done..."))
 }
-
-
